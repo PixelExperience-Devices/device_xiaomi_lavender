7c72809265c3729571fb328588df1dfe0d9a1e7b
Dan Cashman <dcashman@google.com>
2017-06-01 15:25:22 -0700

    lavender: Add BOARD_PLAT_[PUBLIC|PRIVATE]_SEPOLICY_DIR
    Move vendor policy to vendor and add a place for system extensions.
    Also add such an extension: a labeling of the qti.ims.ext service.
    
    Bug: 38151691
    Bug: 62041272
    Test: Policy binary identical before and after, except plat_service_contexts
    has new service added.
    Change-Id: Ie4e8527649787dcf2391b326daa80cf1c9bd9d2f
    
    Change-Id: I1493c4c8876c4446a1de46b39942098bf49c79f8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -227,7 +227,9 @@ include device/qcom/sepolicy/sepolicy.mk
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 
 # Treble
 BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
