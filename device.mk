a186b3048ac036f72d64ace47d0affb35a71f3b8
XiNGRZ <chenxingyu92@gmail.com>
2017-07-18 18:59:29 +0800

    whyred: add SOTER for WeChat fingerprint support
    Import from https://github.com/OnePlusOSS/android_frameworks_base
    Branch: oneplus/QC8998_N_7.1
    Last commit:
    
        commit 00d31cf395ec6e395c4ab4a9a06c6a6730d11ddf
        Author: Mahantesh Sanakall <msanak@codeaurora.org>
        Date:   Tue Apr 4 12:17:08 2017 +0530
    
            [PATCH] [All][community] add for community
    
            Plan: as title
            Type: PATCH
            Test: No need to test
    
    Change-Id: Ie3db069da7d274b4b3287fe1f410f7c2aa6cada6

--- device.mk
+++ device.mk
@@ -425,6 +425,13 @@ PRODUCT_PACKAGES += \
 # VNDK
 PRODUCT_PACKAGES += vndk-sp
 
+# Soter
+PRODUCT_PACKAGES += \
+    soter
+
+PRODUCT_BOOT_JARS += \
+    soter
+
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
