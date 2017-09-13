2e186687b2288795abb149f301b39f092459b7f4
Pratham Pratap <prathampratap@codeaurora.org>
2017-09-13 14:26:58 +0530

    whyred: init.qcom.usb.sh: Set 90B8 as USB default composition on msmpeafowl
    This change sets USB default composition as 90B8: DIAG, DUN, RMNET,
    DPL, ADB interfaces on msmpeafowl.
    
    Change-Id: If1b91b48283cd00bb08f2632f227fd99b6efccc2
    Signed-off-by: Pratham Pratap <prathampratap@codeaurora.org>

--- rootdir/bin/init.qcom.usb.sh
+++ rootdir/bin/init.qcom.usb.sh
@@ -165,7 +165,7 @@ case "$usb_config" in
 	              "msm8998" | "sdm660" | "apq8098_latv")
 		          setprop persist.sys.usb.config diag,serial_cdev,rmnet,adb
 		      ;;
-	              "sdm845")
+	              "sdm845" | "msmpeafowl")
 		          setprop persist.sys.usb.config diag,serial_cdev,rmnet,dpl,adb
 		      ;;
 	              *)
