169ff432809348578d5481f2181f366b5025d988
jhenrique09 <jhenrique09.mcz@hotmail.com>
2017-11-12 19:29:06 -0200

    lavender: Introduce Safailnet
    * temporary safetynet haxx until we get enforced

--- device.mk
+++ device.mk
@@ -362,7 +362,8 @@ PRODUCT_PACKAGES += \
     init.qcom.sensors.sh \
     init.target.rc \
     ueventd.qcom.rc \
-    fstab.qcom
+    fstab.qcom \
+    init.safailnet.rc
 
 # RCS
 PRODUCT_PACKAGES += \
