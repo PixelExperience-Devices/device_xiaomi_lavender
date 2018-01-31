fa34b04ed26485b42c393f734d1363ae70cf5b8b
Edwin Wong <edwinwong@google.com>
2018-01-31 10:29:05 -0800

    whyred: Add SELinux policy for clearkey HIDL service.
    Convert clearkey plugin to HIDL to support drm HAL v1.1.
    Add SELinux policy for android.hardware.drm@1.1-service.clearkey.
    
    Test: CTS test
      ANDROID_BUILD_TOP= ./android-ccts-tradefed run cts-dev
        --module CtsMediaTestCases
        -t android.media.cts.ClearKeySystemTest#testClearKeyPlaybackCenc
    
    bug: 69635855
    Change-Id: I61e9c272c2a2788fd07d5c12921d28c785661b77

--- device.mk
+++ device.mk
@@ -186,7 +186,8 @@ PRODUCT_PACKAGES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service
+    android.hardware.drm@1.0-service \
+    android.hardware.drm@1.1-service.clearkey
 
 # FBE support
 PRODUCT_COPY_FILES += \
