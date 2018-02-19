a237e227bf1a0f0a647332bed90e521c773c0781
Paul Keith <javelinanddart@gmail.com>
2018-02-19 18:21:29 +0100

    whyred: Use sdfat for exfat
    Change-Id: I660c5b3008636a651a2812fe1739ff8a2ea845cb

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -158,7 +158,7 @@ VSYNC_EVENT_PHASE_OFFSET_NS := 2000000
 TARGET_TAP_TO_WAKE_NODE := "/sys/class/input/input2/wake_gesture"
 
 # EXFAT
-TARGET_EXFAT_DRIVER := exfat
+TARGET_EXFAT_DRIVER := sdfat
 
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
