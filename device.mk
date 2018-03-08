08a9d5ae804ea394c9cf22d3de18d065814216cf
TheScarastic <warabhishek@gmail.com>
2018-03-08 20:15:45 +0530

    lavender: delete goodix data if other slot is taking up space

--- device.mk
+++ device.mk
@@ -136,6 +136,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     init.class_main.sh \
     init.msm.usb.configfs.rc \
+    init.goodix.sh \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
     init.qcom.rc \
