b06b30b89e160fb8459d277824c5a4bbfb113973
TheScarastic <warabhishek@gmail.com>
2018-03-08 20:15:45 +0530

    whyred: delete goodix data if other slot is taking up space
    Change-Id: I1415b7ea3c7c17dd33cf1a05cdc312d4523eea3c

--- device.mk
+++ device.mk
@@ -365,6 +365,7 @@ PRODUCT_PACKAGES += \
 # Ramdisk
 PRODUCT_PACKAGES += \
     init.class_main.sh \
+    init.goodix.sh \
     init.msm.usb.configfs.rc \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
