2673235f98603a9f1f1bcb121e07770b3404803c
Dan Willemsen <dwillemsen@google.com>
2018-04-04 16:49:07 -0700

    lavender: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES
    Bug: 77599627
    Test: lunch  [see entries in list]
    Change-Id: Id44fcfec47eeb979a745f879031f1bec7359a633

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -16,3 +16,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_lavender.mk
+
+COMMON_LUNCH_CHOICES := \
+    aosp_lavender-user \
+    aosp_lavender-userdebug \
+    aosp_lavender-eng
--- vendorsetup.sh
+++
@@ -1,1 +0,0 @@
-add_lunch_combo aosp_lavender-eng
