f5d1a891d10e6dca22b34bcf87e09dc65932395f
GuaiYiHu <guaiyihu@foxmail.com>
2018-04-05 22:10:01 +0800

    whyred: device: Config for appt
    Change-Id: I5dc40fedd99c8ad4f678d31dace2900d75c0ada5

--- device.mk
+++ device.mk
@@ -62,6 +62,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
+# Device uses high-density artwork where available
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
 # Default vendor configuration.
 ifeq ($(ENABLE_VENDOR_IMAGE),)
 ENABLE_VENDOR_IMAGE := true
