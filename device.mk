3b1f7afe24d4f078f4b7c0d0ab3813916c65b078
GuaiYiHu <guaiyihu@foxmail.com>
2018-04-05 22:29:22 +0800

    whyred: Add sec_config from stock
    Change-Id: I56b1afb91606e61b49a03449eb0a10b4270dd0a8

--- device.mk
+++ device.mk
@@ -259,6 +259,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/msm_irqbalance.conf:system/vendor/etc/msm_irqbalance.conf \
     $(LOCAL_PATH)/configs/msm_irqbalance_sdm630.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance_sdm630.conf
 
+# IRSC
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
+
 # WLAN driver configuration file
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini \
