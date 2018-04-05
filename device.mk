5e7c358b011fa7ed3104292caf44b8d77be403d9
GuaiYiHu <guaiyihu@foxmail.com>
2018-04-05 22:39:04 +0800

    whyred: device: Build libjson
    Change-Id: Iba581da74ace500f8eb7b4fb50c521bfa3c2f4be

--- device.mk
+++ device.mk
@@ -293,6 +293,10 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.0-service \
     android.hardware.power@1.0-impl
 
+# QMI
+PRODUCT_PACKAGES += \
+    libjson
+
 # WLAN driver configuration file
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini \
