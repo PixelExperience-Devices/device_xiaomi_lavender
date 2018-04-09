209001e1d8bd38977c23914dfa85eef9228d6f69
Jack Pham <jackp@codeaurora.org>
2018-04-09 11:46:28 -0700

    whyred: init.qcom.usb.sh: remove obsolete targets
    Remove specific initialization for targets which are no longer
    supported. This includes unused parameters such as CDROM lun,
    charging disablement, and HSIC.
    
    Change-Id: I8dd3adae4ee892e8a3912e0eba1b94b2cfd34120

--- rootdir/bin/init.qcom.usb.sh
+++ rootdir/bin/init.qcom.usb.sh
@@ -27,8 +27,6 @@
 # IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 #
-chown -h root.system /sys/devices/platform/msm_hsusb/gadget/wakeup
-chmod -h 220 /sys/devices/platform/msm_hsusb/gadget/wakeup
 
 # Set platform variables
 if [ -f /sys/devices/soc0/hw_platform ]; then
@@ -42,44 +40,6 @@ if [ -f /sys/devices/soc0/machine ]; then
 else
     soc_machine=`cat /sys/devices/system/soc/soc0/machine` 2> /dev/null
 fi
-
-# Get hardware revision
-if [ -f /sys/devices/soc0/revision ]; then
-    soc_revision=`cat /sys/devices/soc0/revision` 2> /dev/null
-else
-    soc_revision=`cat /sys/devices/system/soc/soc0/revision` 2> /dev/null
-fi
-
-#
-# Allow persistent usb charging disabling
-# User needs to set usb charging disabled in persist.usb.chgdisabled
-#
-target=`getprop ro.board.platform`
-usbchgdisabled=`getprop persist.usb.chgdisabled`
-case "$usbchgdisabled" in
-    "") ;; #Do nothing here
-    * )
-    case $target in
-        "msm8660")
-        echo "$usbchgdisabled" > /sys/module/pmic8058_charger/parameters/disabled
-        echo "$usbchgdisabled" > /sys/module/smb137b/parameters/disabled
-	;;
-        "msm8960")
-        echo "$usbchgdisabled" > /sys/module/pm8921_charger/parameters/disabled
-	;;
-    esac
-esac
-
-usbcurrentlimit=`getprop persist.usb.currentlimit`
-case "$usbcurrentlimit" in
-    "") ;; #Do nothing here
-    * )
-    case $target in
-        "msm8960")
-        echo "$usbcurrentlimit" > /sys/module/pm8921_charger/parameters/usb_max_current
-	;;
-    esac
-esac
 
 #
 # Check ESOC for external MDM
@@ -135,12 +95,6 @@ case "$usb_config" in
 		    ;;
 		    *)
 	            case "$target" in
-                      "msm8916")
-		          setprop persist.sys.usb.config diag,serial_smd,rmnet_bam,adb
-		      ;;
-	              "msm8994" | "msm8992")
-	                  setprop persist.sys.usb.config diag,serial_smd,serial_tty,rmnet_ipa,mass_storage,adb
-		      ;;
 	              "msm8996")
 	                  setprop persist.sys.usb.config diag,serial_cdev,serial_tty,rmnet_ipa,mass_storage,adb
 		      ;;
@@ -272,23 +226,7 @@ else
         # Do target specific things
         #
         case "$target" in
-             "msm8974")
-                # Select USB BAM - 2.0 or 3.0
-                echo ssusb > /sys/bus/platform/devices/usb_bam/enable
-             ;;
-             "apq8084")
-                if [ "$baseband" == "apq" ]; then
-                      echo "msm_hsic_host" > /sys/bus/platform/drivers/xhci_msm_hsic/unbind
-                fi
-             ;;
-             "msm8226")
-                if [ -e /sys/bus/platform/drivers/msm_hsic_host ]; then
-                      if [ ! -L /sys/bus/usb/devices/1-1 ]; then
-                          echo msm_hsic_host > /sys/bus/platform/drivers/msm_hsic_host/unbind
-                      fi
-                fi
-             ;;
-             "msm8994" | "msm8992" | "msm8996" | "msm8953")
+             "msm8996" | "msm8953")
                 echo BAM2BAM_IPA > /sys/class/android_usb/android0/f_rndis_qc/rndis_transports
                 echo 131072 > /sys/module/g_android/parameters/mtp_tx_req_len
                 echo 131072 > /sys/module/g_android/parameters/mtp_rx_req_len
@@ -304,78 +242,11 @@ else
 fi
 
 #
-# set module params for embedded rmnet devices
-#
-rmnetmux=`getprop persist.rmnet.mux`
-case "$baseband" in
-    "mdm" | "dsda" | "sglte2")
-        case "$rmnetmux" in
-            "enabled")
-                    echo 1 > /sys/module/rmnet_usb/parameters/mux_enabled
-                    echo 8 > /sys/module/rmnet_usb/parameters/no_fwd_rmnet_links
-                    echo 17 > /sys/module/rmnet_usb/parameters/no_rmnet_insts_per_dev
-            ;;
-        esac
-        echo 1 > /sys/module/rmnet_usb/parameters/rmnet_data_init
-        # Allow QMUX daemon to assign port open wait time
-        chown -h radio.radio /sys/devices/virtual/hsicctl/hsicctl0/modem_wait
-    ;;
-    "dsda2")
-          echo 2 > /sys/module/rmnet_usb/parameters/no_rmnet_devs
-          echo hsicctl,hsusbctl > /sys/module/rmnet_usb/parameters/rmnet_dev_names
-          case "$rmnetmux" in
-               "enabled") #mux is neabled on both mdms
-                      echo 3 > /sys/module/rmnet_usb/parameters/mux_enabled
-                      echo 8 > /sys/module/rmnet_usb/parameters/no_fwd_rmnet_links
-                      echo 17 > write /sys/module/rmnet_usb/parameters/no_rmnet_insts_per_dev
-               ;;
-               "enabled_hsic") #mux is enabled on hsic mdm
-                      echo 1 > /sys/module/rmnet_usb/parameters/mux_enabled
-                      echo 8 > /sys/module/rmnet_usb/parameters/no_fwd_rmnet_links
-                      echo 17 > /sys/module/rmnet_usb/parameters/no_rmnet_insts_per_dev
-               ;;
-               "enabled_hsusb") #mux is enabled on hsusb mdm
-                      echo 2 > /sys/module/rmnet_usb/parameters/mux_enabled
-                      echo 8 > /sys/module/rmnet_usb/parameters/no_fwd_rmnet_links
-                      echo 17 > /sys/module/rmnet_usb/parameters/no_rmnet_insts_per_dev
-               ;;
-          esac
-          echo 1 > /sys/module/rmnet_usb/parameters/rmnet_data_init
-          # Allow QMUX daemon to assign port open wait time
-          chown -h radio.radio /sys/devices/virtual/hsicctl/hsicctl0/modem_wait
-    ;;
-esac
-
-#
-# Add support for exposing lun0 as cdrom in mass-storage
-#
-cdromname="/system/etc/cdrom_install.iso"
-platformver=`cat /sys/devices/soc0/hw_platform`
-case "$target" in
-	"msm8226" | "msm8610" | "msm8916")
-		case $platformver in
-			"QRD")
-				echo "mounting usbcdrom lun"
-				echo $cdromname > /sys/class/android_usb/android0/f_mass_storage/rom/file
-				chmod 0444 /sys/class/android_usb/android0/f_mass_storage/rom/file
-				;;
-		esac
-		;;
-esac
-
-#
 # Initialize RNDIS Diag option. If unset, set it to 'none'.
 #
 diag_extra=`getprop persist.vendor.usb.config.extra`
 if [ "$diag_extra" == "" ]; then
 	setprop persist.vendor.usb.config.extra none
-fi
-
-# soc_ids for 8937
-if [ -f /sys/devices/soc0/soc_id ]; then
-	soc_id=`cat /sys/devices/soc0/soc_id`
-else
-	soc_id=`cat /sys/devices/system/soc/soc0/id`
 fi
 
 # enable rps cpus on msm8937 target
