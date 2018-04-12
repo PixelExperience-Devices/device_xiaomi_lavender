d6bc7a2733bdd8fadb55045413daaa685af8e806
GuaiYiHu <guaiyihu@foxmail.com>
2018-04-12 15:40:09 +0800

    whyred: Move light hal to vendor
    Change-Id: I9ab9b604bc581c89204100f1d7f8ad5cb7d811d6

--- liblight/Android.mk
+++ liblight/Android.mk
@@ -17,11 +17,9 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := lights.$(TARGET_BOARD_PLATFORM)
-
 LOCAL_MODULE_RELATIVE_PATH := hw
-
 LOCAL_MODULE_TAGS := optional
-
+LOCAL_PROPRIETARY_MODULE := true
 LOCAL_SRC_FILES := lights.c
 LOCAL_SHARED_LIBRARIES := liblog libcutils
 
