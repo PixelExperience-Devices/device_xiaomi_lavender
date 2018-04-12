8fc3c5b2fe79b36811311f602abb1e549f135bc2
GuaiYiHu <guaiyihu@foxmail.com>
2018-04-12 14:41:42 +0800

    whyred: device: Build drm packages
    Change-Id: Ie522b94ad997dc86858fd4e46fefcb8c4b32df6c

--- device.mk
+++ device.mk
@@ -230,6 +230,11 @@ PRODUCT_PACKAGES += \
     android.hardware.configstore@1.0-service \
     android.hardware.broadcastradio@1.0-impl
 
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service
+
 # FBE support
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/bin/init.qti.qseecomd.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qti.qseecomd.sh
