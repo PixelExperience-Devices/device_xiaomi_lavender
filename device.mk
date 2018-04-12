48f54f465b1a3051476f7a498fdd653ac92535bc
GuaiYiHu <guaiyihu@foxmail.com>
2018-04-12 14:13:31 +0800

    whyred: device: Build audio hidl hals
    Change-Id: Icdd91f2b432da3cd7ab2e61ef8967e2d483c07c3

--- device.mk
+++ device.mk
@@ -108,7 +108,12 @@ PRODUCT_CHARACTERISTICS := nosdcard
 
 # Audio
 PRODUCT_PACKAGES += \
-    audiod \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio@2.0-service \
+    android.hardware.audio.effect@2.0-impl \
+    android.hardware.audio.effect@2.0-service \
+    android.hardware.soundtrigger@2.0-impl \
+    android.hardware.soundtrigger@2.0-service \
     audio.a2dp.default \
     audio.primary.sdm660 \
     audio.r_submix.default \
