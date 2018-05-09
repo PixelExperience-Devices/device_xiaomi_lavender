2938052d00a736c3f8dbbc16f8543ab81e8453c6
guaiyihu <guaiyihu@foxmail.com>
2018-05-09 17:39:07 +0800

    whyred: board: Disable dex preoptimisation
    Change-Id: I4a7070af1375a52b10d9f5cbcde4a05f924bfff7

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -239,16 +239,8 @@ WIFI_DRIVER_FW_PATH_P2P := "p2p"
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # Enable dex pre-opt to speed up initial boot
-ifeq ($(HOST_OS),linux)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
-      WITH_DEXPREOPT_PIC := true
-      ifneq ($(TARGET_BUILD_VARIANT),user)
-        # Retain classes.dex in APK's for non-user builds
-        DEX_PREOPT_DEFAULT := nostripping
-      endif
-    endif
-endif
+WITH_DEXPREOPT := false
+WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
 
 # inherit from the proprietary version
 -include vendor/xiaomi/whyred/BoardConfigVendor.mk
