6210bfa6314ff20072ddc907d01936a780b56c20
guaiyihu <guaiyihu@foxmail.com>
2018-05-26 18:27:47 +0800

    lavender: keylayout: Map Button Jack keylayout to fix headset button controls
     * input: sdm660-snd-card Button Jack as /devices/soc/soc:sound/sound/card0/input8
    
    Change-Id: I26b4f9dd54856e4b43b9c79c81873641c7b7082b

--- device.mk
+++ device.mk
@@ -64,7 +64,7 @@ PRODUCT_COPY_FILES += \
 
 # Keylayout
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl \
+    $(LOCAL_PATH)/configs/keylayout/sdm660-snd-card_Button_Jack.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/sdm660-snd-card_Button_Jack.kl \
     $(LOCAL_PATH)/configs/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/configs/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl \
 
