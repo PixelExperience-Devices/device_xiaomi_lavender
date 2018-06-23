ac71d817380d5abce827b8d3b4b3cdc96ad5d521
Demon Singur <demonsingur@gmail.com>
2018-06-23 05:22:18 +0000

    lavender: Build separate system and vendor HIDL libs
    Change-Id: If8e2211e643bdb0ff67fb248e7fe33b78077a06e
    Signed-off-by: AmulyaX <amulya.b520@gmail.com>

--- device.mk
+++ device.mk
@@ -229,7 +229,9 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
+    android.hidl.base@1.0_system \
     android.hidl.manager@1.0 \
+    android.hidl.manager@1.0_system \
     android.hidl.manager@1.0-java
 
 # IMS
