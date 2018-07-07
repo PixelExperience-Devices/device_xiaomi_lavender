e02f9f951cf815ac289b249e3bfccc72a3bf9e23
Bruno Martins <bgcngm@gmail.com>
2018-07-07 22:40:59 +0100

    whyred: Remove implicitly built QTI camera shared libraries
     * These shared libs are built when TARGET_USES_QTI_CAMERA_DEVICE
       flag is enabled.
    
    Change-Id: Ifb39b7c2c8de2cbb1f59c6dc492a3763d32f1b7a

--- device.mk
+++ device.mk
@@ -148,8 +148,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     libxml2 \
-    Snap \
-    vendor.qti.hardware.camera.device@1.0_vendor
+    Snap
 
 # CNE
 PRODUCT_PACKAGES += \
