a6ca63fa8808d044ad3b1ebb730737a553468143
guaiyihu <guaiyihu@foxmail.com>
2018-08-27 02:15:46 +0800

    whyred: Set vendor props via PRODUCT_PROPERTY_OVERRIDES
    Change-Id: I51128d934912182833ddf5469662feab6ea5d79f

--- device.mk
+++ device.mk
@@ -23,6 +23,9 @@
 
 $(call inherit-product, vendor/xiaomi/whyred/whyred-vendor.mk)
 
+# Vendor properties
+-include $(LOCAL_PATH)/vendor_prop.mk
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
