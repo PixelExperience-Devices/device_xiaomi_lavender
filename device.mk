c912fd2fabae6ac8c107de4c864e97cdb3092f3b
guaiyihu <guaiyihu@foxmail.com>
2018-08-28 00:18:25 +0800

    Revert "whyred: Switch to prebuilt fingerprint service"
    This reverts commit 22019cf6c98d700a066acf6912c0ba1ff9f28383.

--- device.mk
+++ device.mk
@@ -194,7 +194,8 @@ PRODUCT_COPY_FILES += \
 
 # Fingerprint feature
 PRODUCT_PACKAGES += \
-    fingerprintd
+    fingerprintd \
+    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_sdm660
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
