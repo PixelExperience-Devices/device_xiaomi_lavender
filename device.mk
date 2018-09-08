12b5da4c7c05a4a3a35f38aa0b73a9a2aa419b5e
dianlujitao <dianlujitao@lineageos.org>
2018-09-08 12:50:26 +0800

    lavender: Tweak media profiles
     * Remove unsupported profiles in AOSP from media_profiles_vendor.xml:
       - vga
       - 4kdci
       - timelapsevga
       - timelapse4kdci
       - highspeedcif
       - highspeedvga
       - highspeed4kdci
       - qhd
       - 2k
       - timelapseqhd
       - timelapse2k
       - lpcm
       and use it as media_profiles_V1_0.xml
     * Remove never used CAF media_profiles_V1_0.xml.
    
    Change-Id: I8309af0804caf32ab0870a7ad754b8d41dc9b544

--- device.mk
+++ device.mk
@@ -286,7 +286,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
     $(LOCAL_PATH)/media/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
-    $(LOCAL_PATH)/media/media_profiles_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_vendor.xml \
     $(LOCAL_PATH)/media/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
