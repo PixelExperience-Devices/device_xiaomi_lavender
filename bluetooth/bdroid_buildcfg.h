d6f07b516e5ed7cf5653c743e93cac113ab25c29
guaiyihu <guaiyihu@foxmail.com>
2018-09-14 00:12:56 +0800

    whyred: bluetooth: Set bluetooth name dynamically
    Change-Id: I51529a33ef88fa885aea5f6f50fcacf16e97a741

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -21,7 +21,26 @@
 
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
-#define BTM_DEF_LOCAL_NAME   "Redmi Note 5"
+
+#include <cutils/properties.h>
+#include <string.h>
+
+static inline const char* BtmGetDefaultName()
+{
+    char product_model[PROPERTY_VALUE_MAX];
+    property_get("ro.product.model", product_model, "");
+
+    if (strstr(product_model, "Redmi Note 5"))
+        return "Redmi Note 5";
+    if (strstr(product_model, "Redmi Note 5 Pro"))
+        return "Redmi Note 5 Pro";
+
+    // Fallback to ro.product.model
+    return "";
+}
+#undef PROPERTY_VALUE_MAX
+
+#define BTM_DEF_LOCAL_NAME BtmGetDefaultName()
 #define BLUETOOTH_QTI_SW TRUE
 // Disables read remote device feature
 #define MAX_ACL_CONNECTIONS   16
