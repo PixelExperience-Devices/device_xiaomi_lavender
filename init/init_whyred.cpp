974825a1e761ff8e92f6b169f46f636a073a3c8e
guaiyihu <guaiyihu@foxmail.com>
2018-09-13 23:14:49 +0800

    whyred: libinit: Set product model dynamically
    Change-Id: I048cc2589ff6ff56f5545e09eb84b81bdf085adf

--- init/init_whyred.cpp
+++ init/init_whyred.cpp
@@ -26,6 +26,7 @@
    IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include <fstream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -33,6 +34,8 @@
 #include <android-base/file.h>
 #include <android-base/properties.h>
 #include <android-base/strings.h>
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
 
 #include "property_service.h"
 #include "vendor_init.h"
@@ -41,6 +44,22 @@ using android::base::GetProperty;
 using android::base::ReadFileToString;
 using android::base::Trim;
 using android::init::property_set;
+
+void property_override(char const prop[], char const value[])
+{
+    prop_info *pi;
+     pi = (prop_info*) __system_property_find(prop);
+    if (pi)
+        __system_property_update(pi, value, strlen(value));
+    else
+        __system_property_add(prop, strlen(prop), value, strlen(value));
+}
+ void property_override_dual(char const system_prop[],
+        char const vendor_prop[], char const value[])
+{
+    property_override(system_prop, value);
+    property_override(vendor_prop, value);
+}
 
 static void init_alarm_boot_properties()
 {
@@ -76,6 +95,24 @@ static void init_alarm_boot_properties()
     }
 }
 
+static void init_setup_model_properties()
+{
+    std::ifstream fin;
+    std::string buf;
+
+    fin.open("/proc/cmdline");
+    while (std::getline(fin, buf, ' '))
+        if (buf.find("androidboot.hwc") != std::string::npos)
+            break;
+    fin.close();
+
+    if (buf.find("CN") != std::string::npos) {
+        property_override_dual("ro.product.model", "ro.vendor.product.model", "Redmi Note 5");
+    } else {
+        property_override_dual("ro.product.model", "ro.vendor.product.model",  "Redmi Note 5 Pro");
+    }
+}
+
 void vendor_load_properties()
 {
     std::string platform;
@@ -85,4 +122,5 @@ void vendor_load_properties()
         return;
 
     init_alarm_boot_properties();
+    init_setup_model_properties();
 }
