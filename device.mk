a97f3e62dd194e73a4a1c2809156990e918a48f2
guaiyihu <guaiyihu@foxmail.com>
2018-09-20 09:55:47 +0800

    whyred: device: Remove non-existent android.hardware.audio.effect@2.0-service
    Change-Id: Iea3cb72c886470b5dc4a51365db4bd151601360c

--- device.mk
+++ device.mk
@@ -90,7 +90,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@4.0-impl:32 \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@4.0-impl:32 \
-    android.hardware.audio.effect@2.0-service \
     android.hardware.soundtrigger@2.1-impl:32 \
     audio.a2dp.default \
     audio.primary.sdm660 \
