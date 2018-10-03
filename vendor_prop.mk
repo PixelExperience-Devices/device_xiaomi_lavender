baf0c0d0341a5ba1e95076627762174a0712b63f
GuaiYiHu <guaiyihu@foxmail.com>
2018-10-04 00:46:51 +0800

    whyred: Drop unused subsystem restart blobs
    Change-Id: Ia05ece8fb24240d6d5a97ca0b9f707221290969d

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -328,10 +328,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.radio.snapshot_enabled=1 \
     persist.vendor.radio.snapshot_timer=5
 
-#enable all system restart_level to relative
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.sys.ssr.restart_level=ALL_ENABLE
-
 #Ignore the wrong IWLAN report when UE in W/G mode.
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.data_con_rprt=1
