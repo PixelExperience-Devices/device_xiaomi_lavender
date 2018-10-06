c406fba2cefca685542395eb6e84525dbd336598
GuaiYiHu <guaiyihu@foxmail.com>
2018-10-07 00:18:38 +0800

    whyred: Drop unused msm_irqbalance_sdm630.conf
    Change-Id: Ie9eebdc2069f3003325d79907b1279ef91cd01b5

--- device.mk
+++ device.mk
@@ -269,8 +269,7 @@ PRODUCT_PACKAGES += \
 
 # IRQ
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf \
-    $(LOCAL_PATH)/configs/msm_irqbalance_sdm630.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance_sdm630.conf
+    $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
 
 # IRSC
 PRODUCT_COPY_FILES += \
