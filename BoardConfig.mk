3436cbf9795b2fafe473327cc152a95e98e3bb76
LuK1337 <priv.luk@gmail.com>
2018-10-14 14:47:55 +0200

    violet: Unset PRODUCT_FULL_TREBLE_OVERRIDE
    * It's not needed since it's automatically set for any devices
      shipped with 26+.
    
    Change-Id: I59e17fe3774af63dbea424be6206b77e0e6050ba
    Signed-off-by: Atman <masteratman@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -189,7 +189,6 @@ include device/qcom/sepolicy/sepolicy.mk
 # Treble
 BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
 BOARD_VNDK_VERSION := current
-PRODUCT_FULL_TREBLE_OVERRIDE := true
 PRODUCT_VENDOR_MOVE_ENABLED := true
 
 # WiFi
