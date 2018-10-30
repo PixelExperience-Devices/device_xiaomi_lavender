1e95fddcd0bd047f83a52c1a2dbbf2daecd6ef20
Santosh Mardi <gsantosh@codeaurora.org>
2018-10-30 16:11:01 +0530

    lavender: init.qcom.post_boot.sh: enable bw_hwmon governor for NPU
    Enable bw_hwmon governor for NPU hardware to monitor
    the traffic between NPU and DDR for moorea target.
    
    Change-Id: If6ec92b1778b0c6f25d7812758234f0d8ac67b53

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -3026,6 +3026,23 @@ case "$target" in
 	            echo 1600 > $llccbw/bw_hwmon/idle_mbps
 	        done
 
+		for npubw in $device/*npu-npu-ddr-bw/devfreq/*npu-npu-ddr-bw
+		do
+		    echo 1 > /sys/devices/virtual/npu/msm_npu/pwr
+		    echo "bw_hwmon" > $npubw/governor
+		    echo 40 > $npubw/polling_interval
+		    echo "1144 1720 2086 2929 3879 5931 6881" > $npubw/bw_hwmon/mbps_zones
+		    echo 4 > $npubw/bw_hwmon/sample_ms
+		    echo 80 > $npubw/bw_hwmon/io_percent
+		    echo 20 > $npubw/bw_hwmon/hist_memory
+		    echo 10 > $npubw/bw_hwmon/hyst_length
+		    echo 30 > $npubw/bw_hwmon/down_thres
+		    echo 0 > $npubw/bw_hwmon/guard_band_mbps
+		    echo 250 > $npubw/bw_hwmon/up_scale
+		    echo 0 > $npubw/bw_hwmon/idle_mbps
+		    echo 0 > /sys/devices/virtual/npu/msm_npu/pwr
+		done
+
 	        #Enable mem_latency governor for L3, LLCC, and DDR scaling
 	        for memlat in $device/*cpu*-lat/devfreq/*cpu*-lat
 	        do
