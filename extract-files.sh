ce34dc058ceb05fd92494494a59d2026db6bacd0
Akhil Narang <akhilnarang.1999@gmail.com>
2018-11-18 17:46:07 +0530

    whyred: proprietary: Move goodix firmware to /vendor/firmware
    Edit libgf_ca.so to look in /vendor/firmware instead of /system/etc/firmware as it only checked in
    /system/etc/firmware and /firmware/image
    
    Change-Id: I4f94467b4841107ec061138ce9f155c1c312f98e
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -61,10 +61,12 @@ extract "$MY_DIR"/proprietary-files-twrp.txt "$SRC" "$SECTION"
 TWRP_QSEECOMD="$MK_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/recovery/root/sbin/qseecomd
 TWRP_GATEKEEPER="$MK_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/recovery/root/sbin/android.hardware.gatekeeper@1.0-service
 TWRP_KEYMASTER="$MK_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/recovery/root/sbin/android.hardware.keymaster@3.0-service
+GOODIX="$MK_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/vendor/lib64/libgf_ca.so
 
 sed -i "s|/system/bin/linker64|/sbin/linker64\x0\x0\x0\x0\x0\x0|g" "$TWRP_QSEECOMD"
 sed -i "s|/system/bin/linker64|/sbin/linker64\x0\x0\x0\x0\x0\x0|g" "$TWRP_GATEKEEPER"
 sed -i "s|/system/bin/linker64|/sbin/linker64\x0\x0\x0\x0\x0\x0|g" "$TWRP_KEYMASTER"
+sed -i "s|/system/etc/firmware|/vendor/firmware\x0\x0\x0\x0|g" $GOODIX
 
 BLOB_ROOT="$MK_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
 patchelf --set-soname libicuuc-v27.so $BLOB_ROOT/vendor/lib/libicuuc-v27.so
