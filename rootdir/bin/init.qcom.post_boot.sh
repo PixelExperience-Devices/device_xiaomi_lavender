e468f175114c713efca7c72d1b3c8c041d75eda4
Tharun Kumar Merugu <mtharu@codeaurora.org>
2018-11-27 16:28:59 +0530

    lavender: init.qcom.post_boot.sh: change cdsp-cdsp-l3-lat governor to cdspl3
    Enable cdspl3 governor for cdsp-cdsp-l3-lat device for SM7150.
    cdspl3 governor accepts CPU L3 clock frequency requests from cdsprm
    module. This improves CDSP applications accessing io-coherent
    buffers on DDR.
    
    Change-Id: I038b1731012c5d072f1bff6214398ad2124281e1

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -3063,6 +3063,12 @@ case "$target" in
 	            echo 400 > $memlat/mem_latency/ratio_ceil
                 done
 
+                #Enable cdspl3 governor for L3 cdsp nodes
+                for l3cdsp in $device/*cdsp-cdsp-l3-lat/devfreq/*cdsp-cdsp-l3-lat
+                do
+                    echo "cdspl3" > $l3cdsp/governor
+                done
+
 	        #Enable compute governor for gold latfloor
 	        for latfloor in $device/*cpu*-ddr-latfloor*/devfreq/*cpu-ddr-latfloor*
 	        do
