d9be83ed3a6eaca804aa302f591fb8a66da28639
Paul McLean <pmclean@google.com>
2018-12-17 08:14:18 -0700

    lavender: Adding Audio HAL V5: Direction API
    Change-Id: I4251b3853d00308dbd988625f8e7466a66b823fa
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- device.mk
+++ device.mk
@@ -90,9 +90,9 @@ PRODUCT_BOOT_JARS += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@4.0-impl:32 \
+    android.hardware.audio@5.0-impl:32 \
     android.hardware.audio@2.0-service \
-    android.hardware.audio.effect@4.0-impl:32 \
+    android.hardware.audio.effect@5.0-impl:32 \
     android.hardware.soundtrigger@2.1-impl:32 \
     audio.a2dp.default \
     audio.primary.sdm660 \
