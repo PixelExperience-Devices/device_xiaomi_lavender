e6f6dfa0f62260651dacd4db16ef7a3ba6d8023a
GuaiYiHu <guaiyihu@foxmail.com>
2019-01-21 23:09:33 +0800

    whyred: Update to MIUI V10.2.2.0.OEICNXM
     * Update build fingerprint
     * Update vendor security patch level
     * Update audio config
    
    Change-Id: If927cd32ca08f3684ee071b2b2a734c74592481b

--- mk_whyred.mk
+++ mk_whyred.mk
@@ -37,7 +37,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="whyred" \
     PRODUCT_NAME="whyred" \
-    BUILD_FINGERPRINT="xiaomi/whyred/whyred:8.1.0/OPM1.171019.011/V10.2.1.0.OEICNFK:user/release-keys" \
-    PRIVATE_BUILD_DESC="whyred-user 8.1.0 OPM1.171019.011 V10.2.1.0.OEICNFK release-keys"
+    BUILD_FINGERPRINT="xiaomi/whyred/whyred:8.1.0/OPM1.171019.011/V10.2.2.0.OEICNXM:user/release-keys" \
+    PRIVATE_BUILD_DESC="whyred-user 8.1.0 OPM1.171019.011 V10.2.2.0.OEICNXM release-keys"
 
 TARGET_VENDOR := Xiaomi
