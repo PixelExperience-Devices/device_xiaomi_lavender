29cd4f0562438f730b760ef0448445dc79662cb7
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-02-01 23:33:54 -0200

    lavender: Dirac fixes
    * Pause/play music stream to get effects applied
    * Rename setMusic to setEnabled
    * Increase "BOOT_COMPLETED" broadcast priority
    
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -36,7 +36,7 @@ public class BootCompletedReceiver extends BroadcastReceiver {
             if (DEBUG) Log.d(TAG, "Starting Doze service");
             DozeUtils.startService(context);
         }
-        DiracUtils.initialize();
+        DiracUtils.initialize(context);
     }
 
 }
