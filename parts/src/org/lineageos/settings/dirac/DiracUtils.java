dfecc31d6b683054482661b8efb251e05d60c71c
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-02-02 14:31:51 -0200

    lavender: dirac: Only call setEnabled on boot completed
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- parts/src/org/lineageos/settings/dirac/DiracUtils.java
+++ parts/src/org/lineageos/settings/dirac/DiracUtils.java
@@ -47,6 +47,9 @@ public final class DiracUtils {
         mContext = context;
         mMediaSessionManager = (MediaSessionManager) context.getSystemService(Context.MEDIA_SESSION_SERVICE);
         mDiracSound = new DiracSound(0, 0);
+    }
+
+    public void onBootCompleted() {
         setEnabled(mDiracSound.getMusic() == 1);
         mDiracSound.setHeadsetType(mDiracSound.getHeadsetType());
         setLevel(getLevel());
