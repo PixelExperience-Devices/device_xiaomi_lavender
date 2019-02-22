aaf1c061826269660c3a27c30de30e1100fbdaa4
dianlujitao <dianlujitao@lineageos.org>
2019-02-22 23:36:41 +0800

    whyred: Add instagram to HAL1 list
    Change-Id: I6006fdd7bf7df530238827fc2e488ee47c72fbd8

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -222,7 +222,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # HAL1 apps list
 PRODUCT_PROPERTY_OVERRIDES += \
-    vendor.camera.hal1.packagelist=com.whatsapp,com.intsig.camscanner
+    vendor.camera.hal1.packagelist=com.whatsapp,com.intsig.camscanner,com.instagram.android
 
 #
 # PROPERTIES FOR IMS
