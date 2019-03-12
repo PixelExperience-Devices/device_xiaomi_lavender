b3d80d74e3e506a77986318c38de9449e2692aab
Yuanyuan Liu <yuanliu@codeaurora.org>
2019-03-12 11:20:57 -0700

    lavender: device: Add wpa_cli
    Add wpa_cli to PRODUCT_PACKAGES. This binary is needed for WLAN.
    
    CRs-Fixed: 2414661
    Change-Id: I0f34a6e8175d80b1b4050ee4df64c0a7334ad110

--- device.mk
+++ device.mk
@@ -456,7 +456,8 @@ PRODUCT_PACKAGES += \
     wificond \
     wifilogd \
     wpa_supplicant \
-    wpa_supplicant.conf
+    wpa_supplicant.conf \
+    wpa_cli
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
