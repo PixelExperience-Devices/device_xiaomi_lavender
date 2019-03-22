7f06761a83b9dee74ca9c02f8480be4df63ffa48
GuaiYiHu <guaiyihu@foxmail.com>
2019-03-22 10:35:47 +0800

    whyred: Update qti configs from caf
     * Also move them to system
    
    Change-Id: I561da9ac4019c20a9413d3046f24db413f7c3153

--- device.mk
+++ device.mk
@@ -333,8 +333,8 @@ PRODUCT_COPY_FILES += \
 
 # QCOM
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/privapp-permissions-qti.xml \
-    $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/qti_whitelist.xml
+    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml \
+    $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
 
 
 # QMI
