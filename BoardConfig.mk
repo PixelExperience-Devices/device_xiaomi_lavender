396477e5fa87731268e50f5097115004fbfdb489
Artem Borisov <dedsa2002@gmail.com>
2019-03-23 13:26:38 +0530

    lavender: board: Enable full dexpreopt
    Change-Id: I61e71a20bbfc17998bd0c8bade7ee98faad15524

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -282,7 +282,7 @@ ifeq ($(HOST_OS),linux)
   ifneq ($(TARGET_BUILD_VARIANT),eng)
     ifeq ($(WITH_DEXPREOPT),)
       WITH_DEXPREOPT := true
-      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
+      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := false
     endif
   endif
 endif
