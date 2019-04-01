82f6e93e10f994641a11c00a458a4a01ce2e1980
subdragonzj <zj502407679@gmail.com>
2019-04-01 19:46:09 +0800

    lavender: add persist symlink

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -208,6 +208,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 BOARD_VENDORIMAGE_PARTITION_SIZE := 838860800
 BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_ROOT_EXTRA_SYMLINKS := \
+    /mnt/vendor/persist:/persist \
     /vendor/dsp:/dsp \
     /vendor/firmware_mnt:/firmware \
     /vendor/bt_firmware:/bt_firmware
