e7231c004c88ee6ff09243356c3873719e5057ea
subdragonzj <zj502407679@gmail.com>
2019-04-03 00:27:32 +0800

    lavender: init product model for Redmi Note 7

--- init/init_lavender.cpp
+++ init/init_lavender.cpp
@@ -67,9 +67,9 @@ static void init_setup_model_properties()
     fin.close();
 
     if (buf.find("CN") != std::string::npos) {
-        property_override_dual("ro.product.model", "ro.vendor.product.model", "Redmi Note 5");
+        property_override_dual("ro.product.model", "ro.vendor.product.model", "Redmi Note 7");
     } else {
-        property_override_dual("ro.product.model", "ro.vendor.product.model",  "Redmi Note 5 Pro");
+        property_override_dual("ro.product.model", "ro.vendor.product.model",  "Redmi Note 7");
     }
 }
 
