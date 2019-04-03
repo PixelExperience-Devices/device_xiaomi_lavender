4e2efd26a6cea06d383be48701a65dee7d750ded
subdragonzj <zj502407679@gmail.com>
2019-04-03 23:11:07 +0800

    lavender: use product_launched_with_p.mk

--- mk_lavender.mk
+++ mk_lavender.mk
@@ -17,7 +17,7 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Inherit from lavender device
 $(call inherit-product, device/xiaomi/lavender/device.mk)
