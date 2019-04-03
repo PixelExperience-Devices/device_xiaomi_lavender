9a4a21e9a2e80547e658d47d52c2772744ea9c72
GuaiYiHu <guaiyihu@foxmail.com>
2019-04-03 06:41:44 +0000

    violet: bluetooth: include: Adapt for violet
    - Ammending authorship to avoid controversy.
    
    Signed-off-by: Atman <masteratman@gmail.com>

--- bluetooth/include/bdroid_buildcfg.h
+++ bluetooth/include/bdroid_buildcfg.h
@@ -23,44 +23,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#include <cutils/properties.h>
-#include <string.h>
-
-#include "osi/include/osi.h"
-
-typedef struct {
-    const char *product_device;
-    const char *product_model;
-} device_t;
-
-static const device_t devices[] = {
-    {"beryllium", "Pocophone F1"},
-    {"dipper", "Xiaomi Mi 8"},
-    {"equuleus", "Xiaomi Mi 8 Pro"},
-    {"perseus", "Xiaomi Mi MIX 3"},
-    {"polaris", "Xiaomi Mi MIX 2S"},
-    {"ursa", "Xiaomi Mi 8 Explorer Edition"},
-};
-
-static inline const char *BtmGetDefaultName()
-{
-    char product_device[PROPERTY_VALUE_MAX];
-    property_get("ro.product.device", product_device, "");
-
-    for (unsigned int i = 0; i < ARRAY_SIZE(devices); i++) {
-        device_t device = devices[i];
-
-        if (strcmp(device.product_device, product_device) == 0) {
-            return device.product_model;
-        }
-    }
-
-    // Fallback to ro.product.model
-    return "";
-}
-
-#define BTM_DEF_LOCAL_NAME BtmGetDefaultName()
-#undef PROPERTY_VALUE_MAX
+#define BTM_DEF_LOCAL_NAME "Redmi Note 7 Pro"
 // Disables read remote device feature
 #define MAX_ACL_CONNECTIONS   16
 #define MAX_L2CAP_CHANNELS    16
