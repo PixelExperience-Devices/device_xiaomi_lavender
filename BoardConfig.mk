b8ca830590cb92e9df9a963712017edb900c2042
Atman <masteratman@gmail.com>
2019-04-05 09:51:23 +0000

    violet: BoardConfig: Enable Android Verified Boot
    Signed-off-by: Atman <masteratman@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,6 +27,9 @@ TARGET_USES_64_BIT_BINDER := true
 TARGET_BOOTLOADER_BOARD_NAME := sm6150
 TARGET_NO_BOOTLOADER := true
 
+# Android Verified Boot
+BOARD_AVB_ENABLE := true
+
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0xA84000 androidboot.hardware=qcom androidboot.console=ttyMSM0 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 service_locator.enable=1 swiotlb=2048 androidboot.configfs=true firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7 androidboot.usbcontroller=a600000.dwc3
