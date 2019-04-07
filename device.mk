a78b05a514040d55e23215b866416200d75ab5fa
subdragonzj <zj502407679@gmail.com>
2019-04-07 17:42:19 +0800

    lavender: update blobs list

--- device.mk
+++ device.mk
@@ -148,7 +148,6 @@ PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0_32 \
     android.hardware.camera.provider@2.4-impl:32 \
     android.hardware.camera.provider@2.4-service \
-    libxml2 \
     Snap
 
 # CNE
