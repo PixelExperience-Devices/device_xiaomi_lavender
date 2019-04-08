81731faab6b0daa32f544c812a11aa3ef0213a23
Bowgo Tsai <bowgotsai@google.com>
2019-04-08 18:28:48 +0800

    lavender: Adding GSI keys
    Change-Id: I112ece91566e6cea066cd637bfed5cae6761ae66
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- device.mk
+++ device.mk
@@ -14,10 +14,13 @@
 # limitations under the License.
 #
 
+$(call inherit-product, vendor/xiaomi/lavender/lavender-vendor.mk)
+
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
-$(call inherit-product, vendor/xiaomi/lavender/lavender-vendor.mk)
+# Installs gsi keys into ramdisk, to boot a GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Vendor properties
 -include $(LOCAL_PATH)/vendor_prop.mk
