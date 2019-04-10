cb41172336f815a6de82be630ae097b71a5dbd05
GuaiYiHu <guaiyihu@foxmail.com>
2019-04-10 13:13:49 +0800

    lavender : Remove android.hidl.manager
     * Also move android.hidl.base@1.0 to system
    
    Change-Id: I06695669f988f28ebfc2016030033f419bb95191
    Signed-off-by: PIPIPIG233666 <2212848813@qq.com>

--- device.mk
+++ device.mk
@@ -278,10 +278,7 @@ PRODUCT_BOOT_JARS += \
 
 # HIDL
 PRODUCT_PACKAGES += \
-    android.hidl.base@1.0 \
-    android.hidl.base@1.0_system \
-    android.hidl.manager@1.0 \
-    android.hidl.manager@1.0_system
+    android.hidl.base@1.0
 
 # IMS
 PRODUCT_PACKAGES += \
