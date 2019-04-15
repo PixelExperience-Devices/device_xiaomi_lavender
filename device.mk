1bee335e092a5c38423d4bade399385698930d27
Jiyong Park <jiyong@google.com>
2019-04-15 14:19:03 +0900

    lavender: Add updatable_apex.mk
    The product abstracts configs that need to be set to support updating of APEXes.
    
    Change-Id: Id5aa944ceba6a6846308f1c18a08af8b3bb38589
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- device.mk
+++ device.mk
@@ -14,6 +14,9 @@
 # limitations under the License.
 #
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 $(call inherit-product, vendor/xiaomi/lavender/lavender-vendor.mk)
 
 # Vendor properties
