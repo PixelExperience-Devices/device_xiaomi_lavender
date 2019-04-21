5cfdeedc392e820a31223c39faf6a26695f7f7bc
subdragonzj <zj502407679@gmail.com>
2019-04-21 17:48:43 +0800

    lavender: update dt2w path

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -163,7 +163,7 @@ VSYNC_EVENT_PHASE_OFFSET_NS := 2000000
 TARGET_ENABLE_MEDIADRM_64 := true
 
 # DT2W
-TARGET_TAP_TO_WAKE_NODE := "/sys/class/input/input2/wake_gesture"
+TARGET_TAP_TO_WAKE_NODE := "/proc/tp_gesture"
 
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
