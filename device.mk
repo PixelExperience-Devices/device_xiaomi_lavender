77779fadfe2628dba430c2badde5689b3772426f
Atman <masteratman@gmail.com>
2019-04-25 10:01:27 +0000

    violet: Build Sensor HIDL
    Signed-off-by: Atman <masteratman@gmail.com>

--- device.mk
+++ device.mk
@@ -166,6 +166,16 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
     $(LOCAL_PATH)/seccomp/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
 
+# Sensor
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-service \
+    libsensorndkbridge
+
+# Sensor Configuration
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
+
 # Telephony
 PRODUCT_PACKAGES += \
     telephony-ext
