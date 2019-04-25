068c085c78c0a6d1ed6a152d1efdb2382e56a5d9
Atman <masteratman@gmail.com>
2019-04-25 11:52:07 +0000

    violet: Import sec_config
    Signed-off-by: Atman <masteratman@gmail.com>

--- device.mk
+++ device.mk
@@ -175,6 +175,10 @@ PRODUCT_PACKAGES += \
     libipanat \
     liboffloadhal
 
+# IRSC
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_sm6150
