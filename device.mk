c89dceca42a94f3b98fc58bd4afac404d2c9e947
Atman <masteratman@gmail.com>
2019-04-25 12:10:19 +0000

    violet: Build Vibrator HIDL
    Signed-off-by: Atman <masteratman@gmail.com>

--- device.mk
+++ device.mk
@@ -250,6 +250,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     lineage.trust@1.0-service
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.0-impl \
+    android.hardware.vibrator@1.0-service
+
 # VNDK-SP
 PRODUCT_PACKAGES += \
     vndk_package
