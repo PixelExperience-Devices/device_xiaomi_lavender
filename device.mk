5b0a3cb7747856913f98e13afbfbc57fabbacbad
Atman <masteratman@gmail.com>
2019-04-25 12:56:05 +0000

    violet: Build Thermal HIDL
    Signed-off-by: Atman <masteratman@gmail.com>

--- device.mk
+++ device.mk
@@ -261,6 +261,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service
+
 # TextClassifier
 PRODUCT_PACKAGES += \
     textclassifier.bundle1
