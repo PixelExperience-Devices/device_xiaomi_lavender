7e285a3ab1af6ffa076b83ea72b5e915f3d7b611
Atman <masteratman@gmail.com>
2019-04-26 16:43:07 +0000

    violet: Import device manifest and compablity
    - From V10.2.8.0.PFHNIXM package
    
    Signed-off-by: Atman <masteratman@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -121,6 +121,7 @@ BOARD_VENDOR_QCOM_LOC_PDK_FEATURE_SET := false
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 
 # Keystore
