196d79f99cd5c413b3ec6707e2a07f99f20ad2ab
Wang Han <416810799@qq.com>
2019-05-13 22:13:25 +0800

    violet: Remove gpio-keys layout
     * Unused.
    
    Change-Id: Ida1537530eace025952a522a20a9811d6a9a376b
    Signed-off-by: Atman <masteratman@gmail.com>

--- device.mk
+++ device.mk
@@ -207,7 +207,6 @@ PRODUCT_PACKAGES += \
 
 # Input
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:system/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:system/usr/keylayout/uinput-goodix.kl
 
