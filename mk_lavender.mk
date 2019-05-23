ea85e65edc9b056a1fa57ae3048aa316cbd76aa5
subdragonzj <zj502407679@gmail.com>
2019-05-24 00:06:07 +0800

    lavender: update build fingerprint to MIUI 9.5.16
    Change-Id: I68e3f9ab2e764b6e1bff5f2f63dde25062edbef3

--- mk_lavender.mk
+++ mk_lavender.mk
@@ -36,7 +36,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="lavender" \
     PRODUCT_NAME="lavender" \
-    BUILD_FINGERPRINT="xiaomi/lavender/lavender:9/PKQ1.180904.001/V10.2.13.0.PFGCNXM:user/release-keys" \
-    PRIVATE_BUILD_DESC="lavender-user 9 PKQ1.180904.001 V10.2.13.0.PFGCNXM release-keys"
+    BUILD_FINGERPRINT="xiaomi/lavender/lavender:9/PKQ1.180904.001/9.5.16:user/release-keys" \
+    PRIVATE_BUILD_DESC="lavender-user 9 PKQ1.180904.001 9.5.16 release-keys"
 
 TARGET_VENDOR := Xiaomi
