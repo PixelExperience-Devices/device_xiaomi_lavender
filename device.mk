94d77735b03d618b41db431302755836541861a9
subdragonzj <zj502407679@gmail.com>
2019-05-26 18:53:40 +0800

    lavender: device.mk: don't only build 64bit sensors hidl hal
    Change-Id: I647e07e659ce8e76785e94f6620e085c6c082f34

--- device.mk
+++ device.mk
@@ -381,7 +381,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl:64 \
+    android.hardware.sensors@1.0-impl \
     android.hardware.sensors@1.0-service
 
 # Telephony
