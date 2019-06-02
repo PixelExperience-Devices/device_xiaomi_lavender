d74a7eb1e8bb070c77c02d8d7723a73bdde8e4f6
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-06-02 12:20:28 +0000

    lavender: Make Android.mk search recursive

--- Android.mk
+++ Android.mk
@@ -1,7 +1,8 @@
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),lavender)
-include $(call all-makefiles-under,$(LOCAL_PATH))
+  subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
+  $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
 
 include $(CLEAR_VARS)
 
