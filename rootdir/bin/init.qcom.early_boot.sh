3e621ef793b4fe7f29df6c1ed23282c3092c6d2a
Dyneteve <sound0020@gmail.com>
2019-06-03 20:47:10 +0300

    lavender: set default lcd density to 420

--- rootdir/bin/init.qcom.early_boot.sh
+++ rootdir/bin/init.qcom.early_boot.sh
@@ -78,7 +78,7 @@ function set_density_by_fb() {
         elif [ $fb_width -ge 1440 ]; then
            setprop vendor.display.lcd_density 560
         elif [ $fb_width -ge 1080 ]; then
-           setprop vendor.display.lcd_density 480
+           setprop vendor.display.lcd_density 420
         elif [ $fb_width -ge 720 ]; then
            setprop vendor.display.lcd_density 320 #for 720X1280 resolution
         elif [ $fb_width -ge 480 ]; then
