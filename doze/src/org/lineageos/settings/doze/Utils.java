23ee59dd9a42736c04e42446be6cdf33c220d8ee
Bruno Martins <bgcngm@gmail.com>
2019-07-09 22:31:00 +0100

    lavender: Doze: Migrate to androidx
    Change-Id: Ifaa67283a3437549a2304088a0ce377b399da19f

--- doze/src/org/lineageos/settings/doze/Utils.java
+++ doze/src/org/lineageos/settings/doze/Utils.java
@@ -21,9 +21,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.UserHandle;
-import android.support.v7.preference.PreferenceManager;
 import android.provider.Settings;
 import android.util.Log;
+import androidx.preference.PreferenceManager;
 
 import static android.provider.Settings.Secure.DOZE_ENABLED;
 
