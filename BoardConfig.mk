1869f4b3c60a87a0f350a9511b1da685ec84847c
Kalpaj Chaudhari <daedroza@gmail.com>
2019-07-14 13:15:26 +0530

    lavender: Optimize SystemUI to avoid janks
    Change-Id: I2bca8f4bad9aad13016fd0e254938f18f154284a
    Signed-off-by: zainarbani <zaintsyariev@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -292,6 +292,7 @@ ifeq ($(HOST_OS),linux)
     endif
   endif
 endif
+PRODUCT_DEXPREOPT_SPEED_APPS += SystemUI
 
 # inherit from the proprietary version
 -include vendor/xiaomi/lavender/BoardConfigVendor.mk
