ec1447e6927ea8579d0a206e63b3516f30252fec
kirillmaster4 <kfilippenok@mail.ru>
2019-08-20 19:06:12 +0500

    lavender: Update build fingerprint to 10.3.9.0.PFGMIXM

--- aosp_lavender.mk
+++ aosp_lavender.mk
@@ -39,8 +39,8 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="lavender" \
     PRODUCT_NAME="lavender" \
-    PRIVATE_BUILD_DESC="lavender-user 9 PKQ1.180904.001 V10.3.6.0.PFGMIXM release-keys"
+    PRIVATE_BUILD_DESC="lavender-user 9 PKQ1.180904.001 V10.3.9.0.PFGMIXM release-keys" \
 
-BUILD_FINGERPRINT := "xiaomi/lavender/lavender:9/PKQ1.180904.001/V10.3.6.0.PFGMIXM:user/release-keys"
+BUILD_FINGERPRINT := "xiaomi/lavender/lavender:9/PKQ1.180904.001/V10.3.9.0.PFGMIXM:user/release-keys"
 
 TARGET_VENDOR := Xiaomi
