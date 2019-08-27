baecb1307a07f0135a7c40fbb8f1b2fbc5c8baf1
Pereira Ricardo <nunocodex@gmail.com>
2019-08-27 15:44:14 +0200

    lavender: remove XiaomiParts

--- device.mk
+++ device.mk
@@ -154,10 +154,6 @@ PRODUCT_PACKAGES += \
     cneapiclient \
     com.quicinc.cne
 
-# Device-specific settings
-PRODUCT_PACKAGES += \
-    XiaomiParts
-
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl:64 \
