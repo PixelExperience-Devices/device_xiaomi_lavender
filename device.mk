5a4920b6a132f73c14000e32deb234898bc35be7
Bruno Rocha <brunoxthe2@hotmail.com>
2019-09-03 20:44:29 -0300

    lavender: move permissions xml to system since now PE is using white list enforced
    bootloop
    log of error:
    PackageManager: Privileged permission android.permission.PACKET_KEEPALIVE_OFFLOAD for package com.quicinc.cne.CNEService - not in privapp-permissions whitelist
    PackageManager: Privileged permission android.permission.INTERACT_ACROSS_USERS for package com.quicinc.cne.CNEService - not in privapp-permissions whitelist
    PackageManager: Un-granting permission android.permission.INTERACT_ACROSS_USERS from package com.quicinc.cne.CNEService (protectionLevel=50 flags=0x3888be4d)
    PackageManager: Privileged permission android.permission.READ_PRECISE_PHONE_STATE for package org.codeaurora.ims - not in privapp-permissions whitelist
    PackageManager: Privileged permission android.permission.INTERACT_ACROSS_USERS for package org.codeaurora.ims - not in privapp-permissions whitelist
    PackageManager: Un-granting permission android.permission.INTERACT_ACROSS_USERS from package org.codeaurora.ims (protectionLevel=50 flags=0x3888be45)
    PackageManager: Privileged permission android.permission.SUBSTITUTE_NOTIFICATION_APP_NAME for package org.codeaurora.ims - not in privapp-permissions whitelist
    PackageManager: Privileged permission android.permission.INTERACT_ACROSS_USERS for package com.qualcomm.qcrilmsgtunnel - not in privapp-permissions whitelist
    PackageManager: Privileged permission android.permission.INTERACT_ACROSS_USERS for package com.qti.dpmserviceapp - not in privapp-permissions whitelist

--- device.mk
+++ device.mk
@@ -315,9 +315,8 @@ PRODUCT_COPY_FILES += \
 
 # QCOM
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/privapp-permissions-qti.xml \
+    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
-
 
 # QMI
 PRODUCT_PACKAGES += \
