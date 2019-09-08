e8984a5d18f3a1c2c5692af2c90e3a88833d7356
dianlujitao <dianlujitao@lineageos.org>
2019-09-08 16:49:08 +0800

    lavender: Fix redefined PROPERTY_VALUE_MAX
    Change-Id: I5cbc5cc1c42a10570ba84ce068d8e26942f3a81b

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -22,12 +22,20 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#include <cutils/properties.h>
+#include <stdint.h>
 #include <string.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+int property_get(const char *key, char *value, const char *default_value);
+#ifdef __cplusplus
+}
+#endif
 
 static inline const char* BtmGetDefaultName()
 {
-    char product_model[PROPERTY_VALUE_MAX];
+    char product_model[92];
     property_get("ro.product.model", product_model, "");
 
     if (strstr(product_model, "Redmi Note 7"))
@@ -36,7 +44,6 @@ static inline const char* BtmGetDefaultName()
     // Fallback to ro.product.model
     return "";
 }
-#undef PROPERTY_VALUE_MAX
 
 #define BTM_DEF_LOCAL_NAME BtmGetDefaultName()
 #define BLUETOOTH_QTI_SW TRUE
