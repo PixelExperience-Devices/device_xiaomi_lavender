043b55a4b3f10b11ca9fa4a1ba173fea21ee08d2
TheStrechh <arriagagarciajc@gmail.com>
2019-09-09 18:56:49 -0500

    lavender: Update overlays

--- device.mk
+++ device.mk
@@ -21,7 +21,8 @@ $(call inherit-product, vendor/xiaomi/lavender/lavender-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
