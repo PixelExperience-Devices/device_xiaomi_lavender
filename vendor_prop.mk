bc2f16a177ce1db56bd3c6c4880a2b0ceea2cbd0
Sujin Panicker <spanic@codeaurora.org>
2019-09-09 10:55:38 +0530

    lavender: audio: Add 48khz sampling rate to spkr prot tx uc
    With 8Khz sampling rate afe fails to fetch calibration
    information.
    sdm660 & msm8998 acdb supports only 48Khz for speaker
    protection calibration tx device, since 8Khz tables were
    not added initially, addition of 8Khz would require
    further tuning.
    This change is to use sample rate based on property, the
    default sample rate is not changed for other targets without
    prop.
    
    Change-Id: I29dc549c699396fc953f3f831cd0ea81ee033ae4

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -135,6 +135,10 @@ persist.audio.fluence.voicecomm=true
 PRODUCT_PROPERTY_OVERRIDES += \
 persist.vendor.bt.aac_frm_ctl.enabled=true
 
+#Set speaker protection cal tx path sampling rate to 48k
+PRODUCT_PROPERTY_OVERRIDES += \
+vendor.audio.spkr_prot.tx.sampling_rate=48000
+
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.audio_hal.in_period_size=144 \
     vendor.audio_hal.period_multiplier=3 \
