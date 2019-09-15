0d60d7d51b81a32b1541e6bd1931679523b21836
TheStrechh <arriagagarciajc@gmail.com>
2019-09-15 23:18:44 +0000

    lavender: add minimal sepolicy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -236,6 +236,7 @@ BOARD_SECCOMP_POLICY := $(DEVICE_PATH)/seccomp
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy-minimal
 #BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 #BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
 #BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
