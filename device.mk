2f4d27750245ce6f725b7c08114b43c4a0fbc90c
TheStrechh <arriagagarciajc@gmail.com>
2019-09-15 16:03:15 +0000

    [DNM]lavender: Disable WfdCommon

--- device.mk
+++ device.mk
@@ -450,8 +450,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
 # Wi-Fi Display
-PRODUCT_BOOT_JARS += \
-    WfdCommon
+#PRODUCT_BOOT_JARS += \
+#    WfdCommon
 
 PRODUCT_PACKAGES += \
     libnl \
