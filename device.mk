d541d196fdc54b81f979b883e62d4a197e3e797e
Dyneteve <sound0020@gmail.com>
2019-09-17 19:58:35 +0300

    lavender: include SafailNet

--- device.mk
+++ device.mk
@@ -369,7 +369,8 @@ PRODUCT_PACKAGES += \
     init.qcom.sensors.sh \
     init.target.rc \
     ueventd.qcom.rc \
-    fstab.qcom
+    fstab.qcom \
+    init.safailnet.rc
 
 # RCS
 PRODUCT_PACKAGES += \
