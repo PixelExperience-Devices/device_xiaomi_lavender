38b6cd7366fbb5981648e32e3e1633c97b13e620
faham1997 <nafidfaham08@gmail.com>
2019-10-01 17:14:14 +0600

    lavender : build hidl packages

--- device.mk
+++ device.mk
@@ -53,6 +53,11 @@ PRODUCT_PACKAGES += \
     android.hardware.health@1.0-impl \
     android.hardware.health@1.0-service
 
+# HIDL
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.manager@1.0
+
 # IDC
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/idc/uinput-fpc.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/uinput-fpc.idc \
