a1b96e7647fe8f2d3281091333b7ffdaa8d800a0
faham1997 <nafidfaham08@gmail.com>
2019-10-01 14:46:32 +0600

    lavender : import system and vendor prop from stock

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,4 +9,6 @@ DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
 
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+
 -include vendor/xiaomi/lavender/BoardConfigVendor.mk
--- device.mk
+++ device.mk
@@ -184,5 +184,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant.conf \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
 
+# Vendor properties
+-include $(LOCAL_PATH)/vendor_prop.mk
+
 # Inherit vendor
 $(call inherit-product, vendor/xiaomi/lavender/lavender-vendor.mk)
