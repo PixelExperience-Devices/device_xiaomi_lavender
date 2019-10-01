0eceeaa9e351721a06c32a8a8378e8e2e917a98b
faham1997 <nafidfaham08@gmail.com>
2019-10-01 17:11:43 +0600

    lavender : Build renderscript hidl hal

--- device.mk
+++ device.mk
@@ -160,6 +160,10 @@ PRODUCT_PACKAGES += \
     rcs_service_api \
     rcs_service_api.xml
 
+# RenderScript HAL
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0-impl
+
 # Seccomp
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/seccomp/configstore@1.1.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/configstore@1.1.policy \
