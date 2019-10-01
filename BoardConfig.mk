7e639e4260e799c38d4fcecd60b3b1eb81e3cde8
faham1997 <nafidfaham08@gmail.com>
2019-10-01 17:39:22 +0600

    lavender : update device arch and platform and remove dependency on DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,5 +1,36 @@
-DEVICE_PATH := device/xiaomi/lavender
+# config.mk
+#
+# Product-specific compile-time definitions.
+#
+
+
 BOARD_VENDOR := xiaomi
+
+DEVICE_PATH := device/xiaomi/lavender
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME :=sdm660
+TARGET_NO_BOOTLOADER := true
+
+# Platform
+BOARD_VENDOR := xiaomi
+TARGET_BOARD_PLATFORM := sdm660
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno512
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a73
+
+TARGET_USES_64_BIT_BINDER := true
 
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
@@ -14,7 +45,6 @@ VENDOR_SECURITY_PATCH := 2019-09-05
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
 
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 
