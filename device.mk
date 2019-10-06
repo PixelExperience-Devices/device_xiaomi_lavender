5773e91ac7fbf705b09e2212c46c0165fbf91624
faham1997 <nafidfaham08@gmail.com>
2019-10-06 17:54:35 +0600

    lavender : import OSS fp hal and sync with MIUI 9.9.3

--- device.mk
+++ device.mk
@@ -151,6 +151,14 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.1-service.clearkey
 
+# Fingerprint feature
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service.lavender
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
+
+
 # FM
 PRODUCT_PACKAGES += \
     FM2 \
