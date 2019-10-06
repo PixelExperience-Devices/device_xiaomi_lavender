0550fa6a6a1e2de0768c3d34d94f9e7d2d6ae707
faham1997 <nafidfaham08@gmail.com>
2019-10-06 20:23:19 +0600

    lavender : product was launched with P

--- lineage_lavender.mk
+++ lineage_lavender.mk
@@ -1,6 +1,14 @@
+ 33 lines (25 sloc) 1.06 KB
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Inherit some common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
