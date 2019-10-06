2d076e8fbf2bf7a1836f2a550c29b839cfb61cd6
faham1997 <nafidfaham08@gmail.com>
2019-10-06 19:43:55 +0600

    lavender : Kang from jason

--- device.mk
+++ device.mk
@@ -16,8 +16,9 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
-
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage 
+ 
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
 
