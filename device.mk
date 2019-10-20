e907109cd5bd34a0dd720460f97fcb1f3eae539b
Ethan Chen <intervigil@gmail.com>
2019-10-20 01:17:32 -0700

    lavender : Remove dead RCS packages
    Change-Id: Ie2c03fa11cb08c7ab900d7b666981d592aba19c7

--- device.mk
+++ device.mk
@@ -409,13 +409,6 @@ PRODUCT_PACKAGES += \
     ueventd.qcom.rc \
     fstab.qcom
 
-# RCS
-PRODUCT_PACKAGES += \
-    rcs_service_aidl \
-    rcs_service_aidl.xml \
-    rcs_service_api \
-    rcs_service_api.xml
-
 # RenderScript HAL
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
