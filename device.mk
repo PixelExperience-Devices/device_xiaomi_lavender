ab88a286ed9da390f79a01c74f7c36aba267e601
NunoCodex <nunocodex@gmail.com>
2019-11-05 03:27:29 +0530

    lavender: Upgrade Power HAL to 1.2

--- device.mk
+++ device.mk
@@ -329,7 +329,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+    android.hardware.power@1.2-service-qti
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
