e42fefcaf5514871eecd4320f17d27c466ab847f
Pereira Ricardo <nunocodex@gmail.com>
2019-11-21 19:08:00 +0100

    [DNM] lavender: don't build FM Radio app

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -166,8 +166,8 @@ TARGET_TAP_TO_WAKE_NODE := "/proc/tp_gesture"
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 
 # FM
-BOARD_HAVE_QCOM_FM := true
-BOARD_HAS_QCA_FM_SOC := "cherokee"
+#BOARD_HAVE_QCOM_FM := true
+#BOARD_HAS_QCA_FM_SOC := "cherokee"
 
 # GPS
 TARGET_NO_RPC := true
--- device.mk
+++ device.mk
@@ -203,10 +203,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
 #FM
-PRODUCT_PACKAGES += \
-    FM2 \
-    libqcomfm_jni \
-    qcom.fmradio
+#PRODUCT_PACKAGES += \
+#    FM2 \
+#    libqcomfm_jni \
+#    qcom.fmradio
 
 # For android_filesystem_config.h
 PRODUCT_PACKAGES += \
