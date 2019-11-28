11a9c6e0b2acff84e326db9f3cc9ce08903e0414
faham1997 <nafidfaham08@gmail.com>
2019-11-28 18:18:24 +0600

    lavender : update overlay for pe compliance with available overlay required from fwb

--- device.mk
+++ device.mk
@@ -16,7 +16,7 @@ TARGET_SCREEN_WIDTH := 1080
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-pe
 
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
