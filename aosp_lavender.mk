a65b526e6440a64ba10d25b776f7f79e993173c8
faham1997 <nafidfaham08@gmail.com>
2019-12-13 14:16:24 +0600

    lavender : [DNM] use pixel fp to bypass safetynet

--- aosp_lavender.mk
+++ aosp_lavender.mk
@@ -32,6 +32,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="lavender" \
     PRIVATE_BUILD_DESC="lavender-user 9 PKQ1.180904.001 V11.0.4.0.PFGMIXM release-keys"
 
-BUILD_FINGERPRINT := "xiaomi/lavender/lavender:9/PKQ1.180904.001/V11.0.4.0.PFGMIXM:user/release-keys"
+BUILD_FINGERPRINT := "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
