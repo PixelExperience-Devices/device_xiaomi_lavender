68ee60002a829138e5bda9ab886d4d043b651ab3
Skylar Chang <>
2019-12-16 15:53:40 +0000

    lavender : init.qcom.rc: FR 53051: remove configs for vendor data ipa mo…
    …dules
    
    As part of this FR we are moving vendor ipa data specific module
    configurations to custom vendor data init rc files

--- rootdir/bin/init.class_main.sh
+++ rootdir/bin/init.class_main.sh
@@ -41,17 +41,11 @@ case "$baseband" in
     stop ril-daemon
     stop vendor.ril-daemon
     stop vendor.qcrild
-    start vendor.ipacm
 esac
 
 case "$baseband" in
     "msm" | "csfb" | "svlte2a" | "mdm" | "mdm2" | "sglte" | "sglte2" | "dsda2" | "unknown" | "dsda3")
     start vendor.qmuxd
-esac
-
-case "$baseband" in
-    "sa8")
-    start vendor.ipacm
 esac
 
 case "$baseband" in
@@ -105,7 +99,6 @@ case "$baseband" in
         start ril-daemon
         start vendor.ril-daemon
     fi
-    start vendor.ipacm
     case "$baseband" in
         "svlte2a" | "csfb")
           start qmiproxy
