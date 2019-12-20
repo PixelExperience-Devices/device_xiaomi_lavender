b9d37085e31e152cf6a56056c439fa3ab8c5a198
Dyneteve <32036579+Dyneteve@users.noreply.github.com>
2019-12-20 10:53:21 +0200

    [DNM]lavender: Set build fingerprint to coral

--- init/init_lavender.cpp
+++ init/init_lavender.cpp
@@ -59,5 +59,5 @@ void vendor_load_properties()
 {
     // fingerprint
     property_override("ro.build.description", "lavender-user 9 PKQ1.180904.001 V11.0.4.0.PFGMIXM release-keys");
-    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys");
+    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:10/QQ1B.191205.011/5974828:user/release-keys");
 }
