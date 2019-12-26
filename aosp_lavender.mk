a18611ec6d699be3a5e75a6a1e8f8171108a7099
faham1997 <nafidfaham08@gmail.com>
2019-12-26 20:08:14 +0000

    lavender : include arcore

--- aosp_lavender.mk
+++ aosp_lavender.mk
@@ -17,6 +17,7 @@ $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_WIFI_EXT := true
+TARGET_INCLUDE_STOCK_ARCORE := true
 
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
