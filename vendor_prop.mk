5fbebb210962c39c4d519371c5517e08f5925785
faham1997 <nafidfaham08@gmail.com>
2020-01-11 03:30:04 +0600

    lavender : Use 4GB dalvik-heap config We are currently using 2GB dalvik-heap config.

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -139,17 +139,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     sdm.debug.disable_skip_validate=1 \
     persist.hwc.enable_vds=1
 
-
-# Dalvik
-PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapgrowthlimit=256m \
-    dalvik.vm.heapstartsize=8m \
-    dalvik.vm.heapsize=512m \
-    dalvik.vm.heaptargetutilization=0.75 \
-    dalvik.vm.heapminfree=512k \
-    dalvik.vm.heapmaxfree=8m \
-    ro.dalvik.vm.native.bridge=0
-
 # DRM
 PRODUCT_PROPERTY_OVERRIDES += \
     drm.service.enabled=true
