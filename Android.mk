74e603904878181bd431974262d9939661d8d14c
dianlujitao <dianlujitao@lineageos.org>
2020-01-30 17:13:44 +0800

    lavender : Add missing ims symlinks
    Change-Id: I77eb1cc4cab9158623c8e383cd8d7c4f183a5791

--- Android.mk
+++ Android.mk
@@ -87,4 +87,14 @@ $(EGL_SYMLINK): $(LOCAL_INSTALLED_MODULE)
 
 ALL_DEFAULT_INSTALLED_MODULES += $(EGL_SYMLINK)
 
+IMS_LIBS := libimscamera_jni.so libimsmedia_jni.so
+IMS_SYMLINKS := $(addprefix $(TARGET_OUT_APPS_PRIVILEGED)/ims/lib/arm64/,$(notdir $(IMS_LIBS)))
+$(IMS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "IMS lib link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /system/lib64/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(IMS_SYMLINKS)
+
 endif
