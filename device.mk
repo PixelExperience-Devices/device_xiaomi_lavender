57792487ba70877be51b230f154fcd092a302227
faham1997 <nafidfaham08@gmail.com>
2020-02-14 04:56:00 +0600

    lavender : uprev config store to 1.1 it is already 1.1 in manifest

--- device.mk
+++ device.mk
@@ -173,7 +173,7 @@ PRODUCT_PACKAGES += \
     memtrack.sdm660
 
 PRODUCT_PACKAGES += \
-    android.hardware.configstore@1.0-service \
+    android.hardware.configstore@1.1-service \
     android.hardware.broadcastradio@1.0-impl
 
 # DRM
