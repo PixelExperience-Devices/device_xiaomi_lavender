7c0139211d751cd90a42bcd03a1a632a1a8210a4
faham1997 <nafidfaham08@gmail.com>
2020-02-14 14:28:03 +0600

    lavender : update build feb fingerprint from coral
    Signed-off-by: faham1997 <nafidfaham08@gmail.com>

--- init/init_lavender.cpp
+++ init/init_lavender.cpp
@@ -59,5 +59,5 @@ void vendor_load_properties()
 {
     // fingerprint
     property_override("ro.build.description", "lavender-user 9 PKQ1.180904.001 V11.0.4.0.PFGMIXM release-keys");
-    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:10/QQ1B.200105.004/6031802:user/release-keys");
+    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:10/QQ1B.200205.002/6084387:user/release-keys");
 }
