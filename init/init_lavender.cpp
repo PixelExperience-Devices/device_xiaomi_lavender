d69d360dcd0f2727e61d151ee73b3583cf46bc88
Nafid Faham <nafidfaham08@gmail.com>
2020-04-07 17:40:29 +0600

    lavender : Update Coral build fp from April

--- init/init_lavender.cpp
+++ init/init_lavender.cpp
@@ -59,5 +59,5 @@ void vendor_load_properties()
 {
     // fingerprint
     property_override("ro.build.description", "lavender-user 9 PKQ1.180904.001 V11.0.5.0.PFGMIXM release-keys");
-    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:10/QQ2A.200305.003/6156912:user/release-keys");
+    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:10/QQ2A.200405.005/6254899:user/release-keys");
 }
