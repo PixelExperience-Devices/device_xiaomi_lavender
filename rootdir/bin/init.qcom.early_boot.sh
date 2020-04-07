4ebf22b1e59db42777d501e700fffde923a525d6
Bipin Kumar <bipikuma@codeaurora.org>
2020-04-07 18:33:44 +0530

    lavender : init: disable UBWC-gralloc for scuba
    Change-Id: Ie1681e1d795a0442ce2ecd8f2d3ed4d3092aecbb

--- rootdir/bin/init.qcom.early_boot.sh
+++ rootdir/bin/init.qcom.early_boot.sh
@@ -355,22 +355,16 @@ case "$target" in
         esac
         ;;
     "bengal")
-        case "$soc_hwplatform" in
+        case "$soc_hwid" in
             441)
                 setprop vendor.media.target.version 2
+                setprop vendor.gralloc.disable_ubwc 1
                 ;;
             *)
                 sku_ver=`cat /sys/devices/platform/soc/5a00000.qcom,vidc/sku_version` 2> /dev/null
                 if [ $sku_ver -eq 1 ]; then
                     setprop vendor.media.target.version 1
                 fi
-                ;;
-        esac
-        ;;
-    "bengal")
-        case "$soc_hwid" in
-            441)
-                setprop vendor.gralloc.disable_ubwc 1
                 ;;
         esac
         ;;
