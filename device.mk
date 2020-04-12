aaea78e36ea55873ac51d5dfb87ca49f435a8eb6
faham1997 <nafidfaham08@gmail.com>
2020-04-12 10:29:41 +0000

    lavender : Build More camera packages

--- device.mk
+++ device.mk
@@ -145,8 +145,10 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.device@3.4 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.5 \
-    android.hardware.camera.provider@2.4-service
-    
+    android.hardware.camera.provider@2.4-service \
+    android.hardware.camera.common@1.0 \
+    vendor.qti.hardware.camera.device@1.0
+
 # Camera  Shims
 PRODUCT_PACKAGES += \
     camera.sdm660_shim
