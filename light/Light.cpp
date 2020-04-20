1bedc23476fee571705169c22ce6c55c37921eb8
faham1997 <nafidfaham08@gmail.com>
2020-04-20 12:43:49 +0600

    Revert "Revert "lavender: light: Properly scale brightness range""
    This reverts commit 8dad5200d00b790a034054b25b1b1cabf5c7c6c2.

--- light/Light.cpp
+++ light/Light.cpp
@@ -113,8 +113,9 @@ static uint32_t getBrightness(const LightState& state) {
 static inline uint32_t scaleBrightness(uint32_t brightness, uint32_t maxBrightness) {
     if (brightness == 0) {
         return 0;
-    }  
-    return brightness * maxBrightness / 0xFF;
+    }
+
+    return (brightness - 1) * (maxBrightness - 1) / (0xFF - 1) + 1;
 }
 
 static inline uint32_t getScaledBrightness(const LightState& state, uint32_t maxBrightness) {
