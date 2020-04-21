84e0cfe282fa91c6f6b14fadcc58233083e54b80
Rahul Janga <rjanga@codeaurora.org>
2020-04-22 03:42:11 +0530

    lavender : init: Set OpenGL ES version to for scuba, kamorta
    Set OpenGL ES version for scuba to 3.1.
    Set OpenGL ES version for kamorta to 3.2.
    
    Change-Id: Ifea9e3762b028b231e7c614ce74848a62ccaa399

--- rootdir/bin/init.qcom.early_boot.sh
+++ rootdir/bin/init.qcom.early_boot.sh
@@ -376,7 +376,12 @@ case "$target" in
                 setprop vendor.fastrpc.disable.cdsprpcd.daemon 1
                 setprop vendor.media.target.version 2
                 setprop vendor.gralloc.disable_ubwc 1
+
+                # 196609 is decimal for 0x30001 to report version 3.1
+                setprop vendor.opengles.version     196609
                 ;;
+            445)
+                setprop vendor.opengles.version     196610
             *)
                 sku_ver=`cat /sys/devices/platform/soc/5a00000.qcom,vidc/sku_version` 2> /dev/null
                 if [ $sku_ver -eq 1 ]; then
