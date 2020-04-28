b5a253355c8fdd5516791e9ac71b1d37009b2efc
faham1997 <nafidfaham08@gmail.com>
2020-04-28 15:49:53 +0600

    Revert "lavender : lights: red -> white"
    This reverts commit f4cf9e66b4920a2b4206727015ec962347d950b8.

--- light/Light.cpp
+++ light/Light.cpp
@@ -25,7 +25,7 @@
 #define LEDS            "/sys/class/leds/"
 
 #define LCD_LED         LEDS "lcd-backlight/"
-#define WHITE_LED       LEDS "white/"
+#define WHITE_LED       LEDS "red/"
 
 #define BLINK           "blink"
 #define BRIGHTNESS      "brightness"
