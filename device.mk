d8633954535b17ebc8dc16fdd4c1c1d14a9ad6c2
Pranav Vashi <neobuddy89@gmail.com>
2020-05-06 03:45:37 +0530

    lavender: Add missing 64-bit interface required for IMS
    Also include only required bitness of the interfaces
    
    Change-Id: I8ef9171cd5d53cfb5ddfaae49b56696940ae6442

--- device.mk
+++ device.mk
@@ -139,12 +139,14 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.device@3.4 \
+    android.hardware.camera.device@3.4:64 \
+    android.hardware.camera.device@3.5:64 \
+    android.hardware.camera.provider@2.4:64 \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.5 \
     android.hardware.camera.provider@2.4-service \
-    android.hardware.camera.common@1.0 \
-    vendor.qti.hardware.camera.device@1.0
+    android.hardware.camera.provider@2.5:64 \
+    libdng_sdk.vendor \
+    vendor.qti.hardware.camera.device@1.0:64
 
 # Camera  Shims
 PRODUCT_PACKAGES += \
