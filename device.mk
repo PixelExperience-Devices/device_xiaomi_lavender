454cee5aaaf902c2b57768314045481a78ff3847
faham1997 <nafidfaham08@gmail.com>
2020-05-07 06:12:01 +0600

    Revert "lavender: Introduce Safailnet"
    This reverts commit 169ff432809348578d5481f2181f366b5025d988.

--- device.mk
+++ device.mk
@@ -403,8 +403,7 @@ PRODUCT_PACKAGES += \
     init.qcom.sensors.sh \
     init.target.rc \
     ueventd.qcom.rc \
-    fstab.qcom \
-    init.safailnet.rc
+    fstab.qcom
 
 # RCS
 PRODUCT_PACKAGES += \
