5eeccf52c9a76f8322b82dbfb5a1ab5240546a8d
Wang Han <416810799@qq.com>
2020-06-07 13:29:14 +0800

    lavender : Remove custom vsync offsets
     * The values are kanged from hammerhead and may introduce
       latency as it is not selected for any of our panels
       and stock does not set these values in the first place.
    
    Change-Id: Iee854ea77d1e8216c8f01aee2f71a178ffcfaf5c

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -227,9 +227,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # SurfaceFlinger
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
-    ro.surface_flinger.max_virtual_display_dimension=4096 \
-    ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
-    ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000
+    ro.surface_flinger.max_virtual_display_dimension=4096
 
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.enable_gl_backpressure=1 \
