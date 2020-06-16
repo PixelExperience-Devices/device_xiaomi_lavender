bcca476684d5a2cbcd10b68f9ae57028d6fda5be
faham1997 <nafidfaham08@gmail.com>
2020-06-16 16:01:34 +0000

    lavender : Include Google Recorder

--- aosp_lavender.mk
+++ aosp_lavender.mk
@@ -15,6 +15,7 @@ TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_WIFI_EXT := true
 TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
 
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
