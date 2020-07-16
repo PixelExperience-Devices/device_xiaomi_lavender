963e3dfca1b9194e3129e53d0b358ee9b59e61b0
Pullakavi Srinivas <spullaka@codeaurora.org>
2020-07-16 23:47:37 +0530

    lavender : display: Enable idle timeout validation for Scuba
    CRs-Fixed: 2719462
    Change-Id: I2e0e92ccb0b531b59cafc54bdac434be9464af1b

--- rootdir/bin/init.qcom.early_boot.sh
+++ rootdir/bin/init.qcom.early_boot.sh
@@ -377,6 +377,7 @@ case "$target" in
                 setprop vendor.fastrpc.disable.cdsprpcd.daemon 1
                 setprop vendor.media.target.version 2
                 setprop vendor.gralloc.disable_ubwc 1
+                setprop vendor.display.enhance_idle_time 1
                 # 196609 is decimal for 0x30001 to report version 3.1
                 setprop vendor.opengles.version     196609
                 sku_ver=`cat /sys/devices/platform/soc/5a00000.qcom,vidc1/sku_version` 2> /dev/null
@@ -388,6 +389,7 @@ case "$target" in
                 #scuba APQ
                 setprop vendor.fastrpc.disable.cdsprpcd.daemon 1
                 setprop vendor.gralloc.disable_ubwc 1
+                setprop vendor.display.enhance_idle_time 1
                 ;;
             *)
                 # default case is for bengal
