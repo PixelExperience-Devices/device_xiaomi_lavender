9e28485ce3cdffd257d57fffd7cbc2e00f65eea5
faham1997 <nafidfaham08@gmail.com>
2020-08-04 13:05:19 +0000

    lavender : Update build fingerprint to QQ3A.200805.001 from coral

--- init/init_lavender.cpp
+++ init/init_lavender.cpp
@@ -59,5 +59,5 @@ void vendor_load_properties()
 {
     // fingerprint
     property_override("ro.build.description", "lavender-user 9 PKQ1.180904.001 V11.0.5.0.PFGMIXM release-keys");
-    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:10/QQ3A.200705.002/6506677:user/release-keys");
+    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:10/QQ3A.200805.001/6578210:user/release-keys");
 }
