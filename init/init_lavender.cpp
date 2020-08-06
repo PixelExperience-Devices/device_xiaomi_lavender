86608be22ad505333d6ecc81993eb2ee711ff004
faham1997 <nafidfaham08@gmail.com>
2020-08-06 21:13:49 +0600

    lavender : Update build desc from  V11.0.1.0.QFGMIXM

--- init/init_lavender.cpp
+++ init/init_lavender.cpp
@@ -58,6 +58,6 @@ void property_override_dual(char const system_prop[], char const vendor_prop[],
 void vendor_load_properties()
 {
     // fingerprint
-    property_override("ro.build.description", "lavender-user 9 PKQ1.180904.001 V11.0.5.0.PFGMIXM release-keys");
+    property_override("ro.build.description", "lavender-user 10 QKQ1.190910.002 V11.0.1.0.QFGMIXM release-keys");
     property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:10/QQ3A.200805.001/6578210:user/release-keys");
 }
