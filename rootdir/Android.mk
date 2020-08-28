dcff17643d73a1672f88ab753090dd3663ca1c03
faham1997 <nafidfaham08@gmail.com>
2020-08-28 21:53:52 +0000

    lavender : Remove remnants of Safailnet

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -106,14 +106,6 @@ LOCAL_SRC_FILES    := bin/init.goodix.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
-include $(CLEAR_VARS) 
-LOCAL_MODULE       := init.safailnet.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.safailnet.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
-include $(BUILD_PREBUILT)
-
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.recovery.qcom.rc
 LOCAL_MODULE_TAGS  := optional
