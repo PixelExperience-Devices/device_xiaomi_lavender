7eed6bea207cbf2eb7f15316b7e2180e07226a58
LuK1337 <priv.luk@gmail.com>
2020-09-18 10:57:02 +0200

    lavender : Update qcom sepolicy include
    Change-Id: I7167575e82eb8c5de2d822643261757edda73a07
    Signed-off-by: faham1997 <nafidfaham08@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -187,7 +187,7 @@ TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 BOARD_SECCOMP_POLICY := $(DEVICE_PATH)/configs/seccomp
 
 # SELinux
-include device/qcom/sepolicy-legacy-um/sepolicy.mk
+include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
