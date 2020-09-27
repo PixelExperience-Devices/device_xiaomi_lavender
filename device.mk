843c9753d74a983b177a0a2cdb3b2903fe9d0b7b
LuK1337 <priv.luk@gmail.com>
2020-09-27 17:45:33 +0200

    lavender : Use libcutils-v29.so for libdpmframework.so
    * v30 one doesn't have `strdup8to16` symbol in it.
    
    Change-Id: Id2b4bb3017bc24c817dbe4b03834960aa1a4b444

--- device.mk
+++ device.mk
@@ -482,6 +482,10 @@ PRODUCT_PACKAGES += \
     vndk_package \
     vndk-ext
 
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v29/arm64/arch-arm-armv8-a/shared/vndk-sp/libcutils.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libcutils-v29.so \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-sp/libcutils.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libcutils-v29.so
+
 # Wallpapers
 PRODUCT_PACKAGES += \
     PixelLiveWallpaperPrebuilt
