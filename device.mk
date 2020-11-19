3dd10bad97f38284ff7196e8ea4a6b06e15fb677
faham1997 <nafidfaham08@gmail.com>
2020-11-19 12:37:56 +0000

    lavender : Build new aidl qti power HAL

--- device.mk
+++ device.mk
@@ -369,7 +369,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service-qti
+	android.hardware.power-service-qti
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
