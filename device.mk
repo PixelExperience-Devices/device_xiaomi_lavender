f06659478e9420732ed2b9164936894ba1e0628d
Michael Bestas <mkbestas@lineageos.org>
2020-11-23 20:47:24 +0200

    lavender: Don't build textclassifier
    * Target no longer exists
    
    Change-Id: I2079d7dbbeb59bbe9cd925a56f6bcc84c8a38d7f

--- device.mk
+++ device.mk
@@ -463,10 +463,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
-# TextClassifier
-PRODUCT_PACKAGES += \
-    textclassifier.bundle1
-
 # Touchscreen
 PRODUCT_PACKAGES += \
     libtinyxml2
