3656efbf647d1af2ea244934ea82003a05d5ea65
faham1997 <nafidfaham08@gmail.com>
2020-11-30 18:41:38 +0600

    Revert "lavender: Force enable mobile network settings v2"
    This reverts commit ae0aa9c4e07954c2e7010e0d9f41dba4e60ac690.

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -191,7 +191,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.backup.ntpServer=0.pool.ntp.org \
-    persist.sys.fflag.override.settings_network_and_internet_v2=true \
     persist.dbg.volte_avail_ovr=1 \
     persist.dbg.vt_avail_ovr=1 \
     persist.dbg.wfc_avail_ovr=1 \
