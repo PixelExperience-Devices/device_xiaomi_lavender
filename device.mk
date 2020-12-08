d57878c7ca1378e586ad3673bf27465f570c5762
Bruno Martins <bgcngm@gmail.com>
2020-12-08 22:03:48 +0000

    lavender: Migrate to Health 2.1 HAL
    Change-Id: Ic6302844ac58c16713b43b6871a705b9b4749a96

--- device.mk
+++ device.mk
@@ -255,8 +255,9 @@ PRODUCT_COPY_FILES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.0-impl \
-    android.hardware.health@2.0-service
+    android.hardware.health@2.1-impl:64 \
+    android.hardware.health@2.1-service
+
 
 # IFAA manager
 PRODUCT_PACKAGES += \
