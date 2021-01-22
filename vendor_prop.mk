f5df428f5e5d995ac6cacac5ba261947e692db50
faham1997 <nafidfaham08@gmail.com>
2021-01-23 05:02:15 +0600

    lavender : Set max frame buffer * remove custom sf gl debugging values ( idk why are these added )

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -225,14 +225,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # SurfaceFlinger
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     ro.surface_flinger.max_virtual_display_dimension=4096
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    debug.sf.disable_backpressure=1 \
-    debug.sf.early_phase_offset_ns=500000 \
-    debug.sf.early_app_phase_offset_ns=500000 \
-    debug.sf.early_gl_phase_offset_ns=3000000 \
-    debug.sf.early_gl_app_phase_offset_ns=15000000
+    debug.sf.disable_backpressure=1 
 
 # System restart
     PRODUCT_PROPERTY_OVERRIDES += \
