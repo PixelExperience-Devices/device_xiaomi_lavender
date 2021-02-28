67e0ae9684a46d63d1764bb390063bef56db8ac4
jhenrique09 <jhenrique09.mcz@hotmail.com>
2021-02-28 17:38:34 -0300

    lavender: Build LiveDisplay

--- device.mk
+++ device.mk
@@ -310,6 +310,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.lavender
 
+# LiveDisplay native
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
