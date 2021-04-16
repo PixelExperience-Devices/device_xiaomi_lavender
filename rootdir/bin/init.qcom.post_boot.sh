60a73ace921cedb28082922fb3ab7ef498845ec7
faham1997 <nafidfaham08@gmail.com>
2021-04-17 05:59:20 +0600

    lavender : Enable zRam

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -552,8 +552,6 @@ else
     # wsf Range : 1..1000 So set to bare minimum value 1.
     echo 1 > /proc/sys/vm/watermark_scale_factor
 
-    configure_zram_parameters
-
     configure_read_ahead_kb_values
 
     enable_swap
