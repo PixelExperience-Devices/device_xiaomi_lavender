fa45d5a62a76c66f4da250a37f7cbb17c607d783
Aaradhay Vijay <inferno964.github@gmail.com>
2022-06-17 17:47:06 +0530

    lavender: Import Audio Configs From lavender-user-10-QKQ1.190910.002-V12.5.3.0.QFGMIXM-release-keys
    Signed-off-by: Aaradhay Vijay <inferno964.github@gmail.com>

--- device.mk
+++ device.mk
@@ -9,6 +9,11 @@ $(call inherit-product, device/xiaomi/sdm660-common/sdm660.mk)
 
 DEVICE_PATH := device/xiaomi/lavender
 
+# Audio
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/audio/audio_platform_info_intcodec.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_intcodec.xml \
+    $(DEVICE_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
+
 # Gatekeeper HAL
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
