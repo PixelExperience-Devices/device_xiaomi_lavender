97c8d5060297e64d62877297fc9c17b62cbd0afc
SebaUbuntu <barezzisebastiano@gmail.com>
2022-06-17 21:13:57 +0530

    lavender: Import extraction scripts
    Change-Id: Ie9695f5013f7f4f33493a868bb526020ec1d441c

---
+++ setup-makefiles.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set -e
+
+export DEVICE=lavender
+export DEVICE_COMMON=sdm660-common
+export VENDOR=xiaomi
+
+"./../../${VENDOR}/${DEVICE_COMMON}/setup-makefiles.sh" "$@"
