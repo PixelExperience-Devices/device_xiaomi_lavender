71b08800d5b079afa1adc0f7fdd8accc3dd50131
SebaUbuntu <barezzisebastiano@gmail.com>
2022-06-17 21:13:57 +0530

    lavender: Import Extraction Scripts
    Signed-off-by: Aaradhay Vijay <inferno964.github@gmail.com>

---
+++ extract-files.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# If we're being sourced by the common script that we called,
+# stop right here. No need to go down the rabbit hole.
+if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
+    return
+fi
+
+set -e
+
+export DEVICE=lavender
+export DEVICE_COMMON=sdm660-common
+export VENDOR=xiaomi
+
+"./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"
