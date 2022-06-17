245f2583a0d41512c3519d78f80b0d9906da326a
Aaradhay Vijay <inferno964.github@gmail.com>
2022-06-17 16:29:11 +0530

    lavender: Initial Device Tree Makefiles
    Signed-off-by: Aaradhay Vijay <inferno964.github@gmail.com>

---
+++ Android.mk
@@ -0,0 +1,12 @@
+#
+# Copyright (C) 2018-2019 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),lavender)
+  subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
+  $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
+endif
---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2018-2019 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_lavender.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_lavender-user \
+    lineage_lavender-userdebug \
+    lineage_lavender-eng
