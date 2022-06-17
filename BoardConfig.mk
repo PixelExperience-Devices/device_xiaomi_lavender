a76ecba6fbebf09b3f5612ff6f74c6993403f4da
Aaradhay Vijay <inferno964.github@gmail.com>
2022-06-17 16:52:02 +0530

    lavender: Specify Partition Sizes
    Signed-off-by: Aaradhay Vijay <inferno964.github@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,5 +9,11 @@ include device/xiaomi/sdm660-common/BoardConfigCommon.mk
 
 DEVICE_PATH := device/xiaomi/lavender
 
+# Partitions
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3640619008
+BOARD_VENDORIMAGE_PARTITION_SIZE := 2080305152
+BOARD_DTBOIMG_PARTITION_SIZE := 8388608
+BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
+
 # Inherit the proprietary files
 include vendor/xiaomi/lavender/BoardConfigVendor.mk
