66d0e88b74de7a9af4a3ca53f3b015852c48be5b
Aaradhay Vijay <inferno964.github@gmail.com>
2022-06-17 16:40:02 +0530

    lavender: Inherit Makefiles From SDM660-Common Tree
    Signed-off-by: Aaradhay Vijay <inferno964.github@gmail.com>

--- device.mk
+++ device.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit from sdm660-common
+$(call inherit-product, device/xiaomi/sdm660-common/sdm660.mk)
+
 DEVICE_PATH := device/xiaomi/lavender
 
 # Inherit the proprietary files
