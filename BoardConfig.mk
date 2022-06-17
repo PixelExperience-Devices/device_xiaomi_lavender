bcd3ab1d839c97815f20edb694e55747e9875ea9
Aaradhay Vijay <inferno964.github@gmail.com>
2022-06-17 16:29:11 +0530

    lavender: Initial device tree makefiles
    Change-Id: I252697590f9c77e40609398c3c6e466227000d23

---
+++ BoardConfig.mk
@@ -0,0 +1,7 @@
+#
+# Copyright (C) 2018-2019 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/xiaomi/lavender
---
+++ lineage_lavender.mk
@@ -0,0 +1,23 @@
+#
+# Copyright (C) 2018-2019 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+
+# Inherit some common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from lavender device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+PRODUCT_BRAND := Xiaomi
+PRODUCT_DEVICE := lavender
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_NAME := lineage_lavender
+PRODUCT_MODEL := Redmi Note 7
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
