922013ca75e105d8f0d2b4aaa985d34ab3699c61
Aaradhay Vijay <inferno964.github@gmail.com>
2022-06-17 21:06:26 +0530

    lavender: Import Camera Props From lavender-user-10-QKQ1.190910.002-V12.5.3.0.QFGMIXM-release-keys *Inherit the makefile
    Signed-off-by: Aaradhay Vijay <inferno964.github@gmail.com>

---
+++ properties.mk
@@ -0,0 +1,16 @@
+# Camera
+PRODUCT_PROPERTY_OVERRIDES += \
+    camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,com.qualcomm.qti.qmmi,com.longcheertel.cit \
+    persist.enable_task_snapshots=false \
+    persist.sys.camera.camera2=true \
+    persist.vendor.camera.HAL3.enabled=1 \
+    persist.vendor.camera.awb.sync=2 \
+    persist.vendor.camera.expose.aux=1 \
+    persist.vendor.camera.imglib.usefdlite=1 \
+    persist.vendor.camera.isp.clock.optmz=0 \
+    persist.vendor.camera.isp.turbo=1 \
+    persist.vendor.camera.manufacturer=Xiaomi \
+    persist.vendor.camera.model=Redmi Note 7 \
+    persist.vendor.camera.preview.ubwc=0 \
+    ro.product.cuptsm=XIAOMI|ESE|02|01 \
+    vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,com.qualcomm.qti.qmmi,com.longcheertel.cit
