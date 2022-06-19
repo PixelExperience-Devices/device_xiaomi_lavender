9257ebf4ef1f3cd8e1e719d29cd6bb47763b8d89
SebaUbuntu <barezzisebastiano@gmail.com>
2022-06-19 18:23:05 +0530

    lavender: Set BOARD_HAVE_IR & QCOM_FM To True
    Signed-off-by: Aaradhay Vijay <inferno964.github@gmail.com>

--- device.mk
+++ device.mk
@@ -17,6 +17,12 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/audio/audio_platform_info_intcodec.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_intcodec.xml \
     $(DEVICE_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
 
+# Consumerir
+BOARD_HAVE_IR := true
+
+# FM
+BOARD_HAVE_QCOM_FM := true
+
 # Gatekeeper HAL
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
