991ba970adc5d61920308b7dcc4473107dc5ccf9
erfanoabdi <erfangplus@gmail.com>
2022-07-08 08:26:37 +0000

    lavender: Add More Default Props To Makefile
    • Dalvik
    • Surfaceflinger (Display)
    
    Signed-off-by: Aaradhay Vijay <inferno964.github@gmail.com>

--- properties.mk
+++ properties.mk
@@ -14,3 +14,17 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.camera.preview.ubwc=0 \
     ro.product.cuptsm=XIAOMI|ESE|02|01 \
     vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,com.qualcomm.qti.qmmi,com.longcheertel.cit
+
+# Dalvik
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapgrowthlimit=256m \
+    dalvik.vm.heapstartsize=8m \
+    dalvik.vm.heapsize=512m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=512k \
+    dalvik.vm.heapmaxfree=8m
+
+# SurfaceFlinger
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.surface_flinger.has_wide_color_display=true \
+    ro.surface_flinger.use_color_management=true
