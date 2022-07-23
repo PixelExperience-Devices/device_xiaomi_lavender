d6806b9af1c5a1d5076f8c2ba7ea2c79e0cb08c6
Aaradhay Vijay <inferno964.github@gmail.com>
2022-07-23 17:19:59 +0000

    lavender: Roll Back To Previous Defconfig
    - Using rebased kernel on lineageOS
    This reverts commit 23b0f9ea48a4d3e541de1f670bd7d806ef34b382.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,7 +32,7 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/xiaomi/lavender
-TARGET_KERNEL_CONFIG := lavender-perf_defconfig
+TARGET_KERNEL_CONFIG := lavender_defconfig
 BOARD_KERNEL_SEPARATED_DTBO := true
 
 # Manifest
