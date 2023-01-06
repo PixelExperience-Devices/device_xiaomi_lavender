68393694984f567931731431cf2bb9d3db55bbcb
Aaradhay <aaradhayvijay964@gmail.com>
2023-01-06 15:52:25 +0000

    Revert "lavender: Override Init System Property"
    This reverts commit 30c14ae5f3a4984ca655496ac518dd6b003b0398.
    
    Change-Id: I5bd344861a571a36e1fbcfddc578d6d77b415067

--- init/init_lavender.cpp
+++ init/init_lavender.cpp
@@ -100,5 +100,4 @@ void vendor_load_properties()
     property_override("dalvik.vm.heaptargetutilization", heaptargetutilization);
     property_override("dalvik.vm.heapminfree", heapminfree);
     property_override("dalvik.vm.heapmaxfree", heapmaxfree);
-    property_override("ro.treble.enabled", "");
 }
